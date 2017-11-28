cef11a3a5e9f9422ee8c7be5b1455fb23b3bdc1e
Leonid Beder <leonid@orbs.network>
2017-11-28 12:34:34 +0200

    Some (minor) refactoring.

--- simulate/src/index.js
+++ simulate/src/index.js
@@ -1,35 +1,35 @@
-const path = require("path");
-const shell = require("shelljs");
+const path = require('path');
+const shell = require('shelljs');
 
 function getTopologies() {
-  const topologiesPath = path.resolve(__dirname, "../../config/topologies");
+  const topologiesPath = path.resolve(__dirname, '../../config/topologies');
   const res = [];
-  shell.ls(topologiesPath).forEach(function (topology) {
+  shell.ls(topologiesPath).forEach((topology) => {
     res.push(topology);
   });
   return res;
 }
 
 function getNodes(topologyName) {
-  const nodesPath = path.resolve(__dirname, "../../config/topologies", topologyName);
+  const nodesPath = path.resolve(__dirname, '../../config/topologies', topologyName);
   const res = [];
-  shell.ls(nodesPath).forEach(function (node) {
+  shell.ls(nodesPath).forEach((node) => {
     res.push(node);
   });
   return res;
 }
 
 function getServices(topologyName, nodeName) {
-  const servicesPath = path.resolve(__dirname, "../../config/topologies", topologyName, nodeName);
+  const servicesPath = path.resolve(__dirname, '../../config/topologies', topologyName, nodeName);
   const res = [];
-  shell.ls(servicesPath).forEach(function (service) {
+  shell.ls(servicesPath).forEach((service) => {
     res.push(service);
   });
   return res;
 }
 
 function getServiceTopologyConfigPath(topologyName, nodeName, serviceName) {
-  return path.resolve(__dirname, "../../config/topologies", topologyName, nodeName, serviceName);
+  return path.resolve(__dirname, '../../config/topologies', topologyName, nodeName, serviceName);
 }
 
 function showUsage() {
@@ -40,7 +40,7 @@ function showUsage() {
 
 function runService(topologyPath) {
   const topology = require(topologyPath);
-  const projectPath = path.resolve(__dirname, "../../projects/", topology.project);
+  const projectPath = path.resolve(__dirname, '../../projects/', topology.project);
   shell.cd(projectPath);
   shell.exec(`node dist/index.js ${topologyPath}`, {async: true});
 }
@@ -55,14 +55,14 @@ function runTopology(topologyName) {
 }
 
 if (!process.argv[2]) {
-  console.log(`ERROR: topology not provided, exiting`);
+  console.error('ERROR: topology not provided, exiting');
   showUsage();
   process.exit();
 }
 
 const topologyName = process.argv[2];
 if (getTopologies().indexOf(topologyName) === -1) {
-  console.log(`ERROR: topology with name '${topologyName}' not found, exiting`);
+  console.error(`ERROR: topology with name '${topologyName}' not found, exiting`);
   showUsage();
   process.exit();
 }
