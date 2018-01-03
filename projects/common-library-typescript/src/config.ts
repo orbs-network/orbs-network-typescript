9a355a7b51da4e15834f6f9ac6c1d2af7428ab7d
Leonid Beder <leonid@orbs.netowrk>
2018-01-03 20:01:55 +0200

    Add running mode awareness.

--- projects/common-library-typescript/src/config.ts
+++ projects/common-library-typescript/src/config.ts
@@ -4,6 +4,7 @@ import * as path from "path";
 export class Config {
   private static readonly NODE_ENV: string = "NODE_ENV";
   private static readonly DEV_ENV: string = "development";
+  private static readonly PROD_ENV: string = "production";
   private static readonly RELATIVE_PATH: string = "../../../config/";
   private static readonly EXT: string = ".json";
 
@@ -17,6 +18,14 @@ export class Config {
 
   public get(key: string): string {
     return nconf.get(key);
+  }
+
+  public isDevelopment(): boolean {
+    return Config.getEnvironment() === Config.DEV_ENV;
+  }
+
+  public isProduction(): boolean {
+    return Config.getEnvironment() === Config.PROD_ENV;
   }
 
   public static getEnvironment(): string {
