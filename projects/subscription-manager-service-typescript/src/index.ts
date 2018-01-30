5e238932984b69ffa697895b704bd32684377ed8
idandush <idanofrat@gmail.com>
2018-01-30 11:51:52 +0200

    Using config.get() again in service bootstrap. Working around nconf.argv() bug by passing env vars

--- projects/subscription-manager-service-typescript/src/index.ts
+++ projects/subscription-manager-service-typescript/src/index.ts
@@ -4,6 +4,6 @@ import SubscriptionManagerService from "./service";
 const server = grpc.subscriptionManagerServer({
   endpoint: topology.endpoint,
   service: new SubscriptionManagerService({
-    ethereumContractAddress: process.argv[3]
+    ethereumContractAddress: config.get("ethereumContractAddress")
   })
 });
