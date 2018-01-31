7d9579a1aa98363c0583960d26d6aea763fef7be
Kirill Maksimov <kirill@orbs.network>
2018-01-31 11:57:23 +0200

    Add environment config for staging, propagate NODE_ENV

--- deploy/bootstrap/bootstrap.sh
+++ deploy/bootstrap/bootstrap.sh
@@ -9,8 +9,10 @@ $(aws ecr get-login --no-include-email --region us-west-2)
 pip install docker-compose
 
 export ENV_FILE=/opt/orbs/.env
+
 echo NODE_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4) >> $ENV_FILE
 echo NODE_NAME=$NODE_NAME >> $ENV_FILE
+echo NODE_ENV=$NODE_ENV >> $ENV_FILE
 echo INSTANCE_ID=$INSTANCE_ID >> $ENV_FILE
 
 export DOCKER_TAG=${DOCKER_TAG-master}
