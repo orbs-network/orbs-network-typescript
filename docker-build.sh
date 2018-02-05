e2a824feed7f402ccc213f5e5a5b61f36ffbb3d9
Leonid Beder <leonid@orbs.netowrk>
2018-02-05 20:28:26 +0200

    Set default DOCKER_IMAGE and DOCKER_TAG

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DOCKER_IMAGE=orbs
-export DOCKER_TAG=$(git rev-parse --abbrev-ref HEAD | sed -e 's/\//-/g')
+export DOCKER_IMAGE=${DOCKER_IMAGE-orbs}
+export DOCKER_TAG=${DOCKER_TAG-$(git rev-parse --abbrev-ref HEAD | sed -e 's/\//-/g')}
 
 docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} .
