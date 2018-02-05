5fae005858f8b809851f11b94d8ca82db03465f3
Leonid Beder <leonid@orbs.com>
2018-02-05 20:28:26 +0200

    Set default DOCKER_IMAGE and DOCKER_TAG

--- docker-test.sh
+++ docker-test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+export DOCKER_IMAGE=${DOCKER_IMAGE-orbs}
+export DOCKER_TAG=${DOCKER_TAG-$(git rev-parse --abbrev-ref HEAD | sed -e 's/\//-/g')}
+
 function generate_dockerfile {
     cat docker-compose.test.yml \
     | sed -e "s/_NODE_NAME_/${NODE_NAME}/g" \
