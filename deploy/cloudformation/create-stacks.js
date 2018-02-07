68d26d103042061739e1c0001b7093c6e14e7864
Leonid Beder <leonid@orbs.netowrk>
2018-02-07 20:05:46 +0200

    Refactor

--- deploy/cloudformation/create-stacks.js
+++ deploy/cloudformation/create-stacks.js
@@ -1,59 +1,59 @@
-const shell = require("shelljs"),
-    fs = require("fs"),
-    stacks = require("./stacks");
+const shell = require('shelljs');
+const fs = require('fs');
+const stacks = require('./stacks');
 
-
-const getParameters = () => require("./parameters");
+const getParameters = () => require('./parameters');
 
 const setParameter = (params, key, value) => {
-    params.map((parameter) => {
-        if (parameter.ParameterKey === key) {
-            parameter.ParameterValue = value;
-        }
-    });
+  params.forEach((param, index) => {
+    if (param.ParameterKey === key) {
+      params[index].ParameterValue = value;
+    }
+  });
 };
 
-const getStackName = (stack) => `orbs-network-${stack.name}`;
+const getStackName = stack => `orbs-network-${stack.name}`;
 
-const deploy = (stack) => {
-    return new Promise((resolve, reject) => {
-        console.log(`Deploying ${stack.name}`);
+const deploy = stack =>
+  new Promise((resolve, reject) => {
+    console.log(`Deploying ${stack.name}`);
 
-        const params = getParameters();
+    const params = getParameters();
 
-        setParameter(params, "ElasticIP", stack["eip-allocation"]);
-        setParameter(params, "NodeName", stack["name"]);
+    setParameter(params, 'ElasticIP', stack['eip-allocation']);
+    setParameter(params, 'NodeName', stack['name']);
 
-        const paramsPath = `${__dirname}/tmp/parameters-${stack.name}.json`;
+    const paramsPath = `${__dirname}/tmp/parameters-${stack.name}.json`;
 
-        fs.writeFileSync(paramsPath, JSON.stringify(params));
+    fs.writeFileSync(paramsPath, JSON.stringify(params));
 
-        shell.exec(`aws cloudformation create-stack --region us-west-2 --template-body file://${__dirname}/cloudformation.yaml --parameters "$(cat ${paramsPath})" --stack-name ${getStackName(stack)}`, (err, response) => {
-            err ? resolve(err) : resolve(response);
-        });
+    shell.exec(`aws cloudformation create-stack --region us-west-2 --template-body file://${__dirname}/cloudformation.yaml --parameters "$(cat ${paramsPath})" --stack-name ${getStackName(stack)}`, (err, response) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response);
+      }
     });
-};
+  });
 
-const remove = (stack) => {
-    return new Promise((resolve, reject) => {
-        console.log(`Removing ${stack.name}`);
+const remove = stack =>
+  new Promise((resolve, reject) => {
+    console.log(`Removing ${stack.name}`);
 
-        shell.exec(`aws cloudformation delete-stack --region us-west-2 --stack-name ${getStackName(stack)}`, (err, response) => {
-            err ? resolve(err) : resolve(response);
-        });
+    shell.exec(`aws cloudformation delete-stack --region us-west-2 --stack-name ${getStackName(stack)}`, (err, response) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response);
+      }
     });
-};
+  });
 
-const removeAll = () => {
-    return Promise.all(stacks.map(remove));
-}
-
-const deployAll = () => {
-    return Promise.all(stacks.map(deploy));
-}
+const removeAll = () => Promise.all(stacks.map(remove));
+const deployAll = () => Promise.all(stacks.map(deploy));
 
 if (process.argv[2] === '--remove-all') {
-    removeAll().then(console.log, console.error);
+  removeAll().then(console.log, console.error);
 } else {
-    deployAll().then(console.log, console.error);
+  deployAll().then(console.log, console.error);
 }
