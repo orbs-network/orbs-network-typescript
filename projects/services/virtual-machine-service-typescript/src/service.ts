d4ef63a229ba554a849a6feea2fe3f49c8e9f8b3
Leonid Beder <leonid@orbs.com>
2018-02-09 23:42:19 +0200

    Convert to service

--- projects/services/virtual-machine-service-typescript/src/service.ts
+++ projects/services/virtual-machine-service-typescript/src/service.ts
@@ -1,3 +1,5 @@
+
+import * as _ from "lodash";
 import bind from "bind-decorator";
 
 import { logger, topology, topologyPeers, grpc, types } from "orbs-common-library";
@@ -5,6 +7,54 @@ import { logger, topology, topologyPeers, grpc, types } from "orbs-common-librar
 import { VirtualMachine } from "orbs-virtual-machine-library";
 
 export default class VirtualMachineService {
-  private storage = topologyPeers(topology.peers).storage;
-  private processor: HardCodedSmartContractProcessor;
+  private virtualMachine: VirtualMachine;
+
+  @bind
+  public async getHeartbeat(rpc: types.GetHeartbeatContext) {
+    logger.debug(`${topology.name}: service '${rpc.req.requesterName}(v${rpc.req.requesterVersion})' asked for heartbeat`);
+    rpc.res = { responderName: topology.name, responderVersion: topology.version };
+  }
+
+  @bind
+  public async executeTransaction(rpc: types.ExecuteTransactionContext) {
+    logger.debug(`${topology.name}: execute transaction ${JSON.stringify(rpc.req)}`);
+
+    // Currently only a "simple" contract type is supported
+    try {
+      const modifiedKeys = await this.virtualMachine.executeTransaction(rpc.req.transaction.sender,
+        rpc.req.transaction.contractAddress, rpc.req.transaction.payload);
+
+      rpc.res = {
+        success: true,
+        modifiedAddressesJson: JSON.stringify(_.fromPairs([...modifiedKeys].map(
+          ([{ contractAddress, key }, value]) => [key, value])))
+      };
+
+    } catch (err) {
+      logger.error("executeTransaction() error: " + err);
+
+      rpc.res = { success: false, modifiedAddressesJson: undefined };
+    }
+  }
+
+  @bind
+  public async callContract(rpc: types.CallContractContext) {
+    logger.debug(`${topology.name}: call contract ${JSON.stringify(rpc.req)}`);
+
+    const result = await this.virtualMachine.callContract(rpc.req.sender, rpc.req.contractAddress, rpc.req.payload);
+
+    rpc.res = {
+      resultJson: JSON.stringify(result)
+    };
+  }
+
+  async main() {
+    logger.info(`${topology.name}: service started`);
+
+    this.virtualMachine = new VirtualMachine();
+  }
+
+  constructor() {
+    setTimeout(() => this.main(), 2000);
+  }
 }
