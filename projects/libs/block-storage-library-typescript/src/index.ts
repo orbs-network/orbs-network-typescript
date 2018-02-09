abe7bae54a4d87d79fee0ac7c488adbe484fb402
Leonid Beder <leonid@orbs.com>
2018-02-09 22:33:33 +0200

    Convert to lib

--- projects/libs/block-storage-library-typescript/src/index.ts
+++ projects/libs/block-storage-library-typescript/src/index.ts
@@ -1,7 +1,1 @@
-import { topology, grpc } from "orbs-common-library";
-import BlockStorageService from "./service";
-
-const server = grpc.blockStorageServer({
-  endpoint: topology.endpoint,
-  service: new BlockStorageService()
-});
+export * from "./block-storage";
