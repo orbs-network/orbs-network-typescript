f06f84504c7cb661d39d0a2ece801a2985fdccde
Leonid Beder <leonid@orbs.com>
2018-02-09 23:56:10 +0200

    Convert to lib

--- projects/libs/transaction-pool-library-typescript/src/index.ts
+++ projects/libs/transaction-pool-library-typescript/src/index.ts
@@ -1,7 +1,1 @@
-import { topology, grpc } from "orbs-common-library";
-import TransactionPoolService from "./service";
-
-const server = grpc.transactionPoolServer({
-  endpoint: topology.endpoint,
-  service: new TransactionPoolService()
-});
+export { TransactionPool } from "./transaction-pool";
