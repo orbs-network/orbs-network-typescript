72dc6f4d2e6c93eb20ea2eaa26060f2c40953e75
Leonid Beder <leonid@orbs.com>
2018-02-10 17:37:29 +0200

    Update build scripts

--- build/src/build-ts.js
+++ build/src/build-ts.js
@@ -8,18 +8,44 @@ require('colors');
 async function main() {
   projects.order.forEach((projectName) => {
     const project = projects[projectName];
-    const projectPath = path.resolve(__dirname, '../../projects/', projectName);
-    if (project.runtime === 'typescript' || project.runtime === 'protobuf') {
-      console.log(`* Building ${projectName}\n`.green);
-      shell.cd(projectPath);
-      const shellStringOutput = shell.exec('./build.sh');
-      if (shellStringOutput.code !== 0) {
-        throw new Error(`Error ${shellStringOutput.code} in ${projectPath}\n`.red);
-      }
+
+    if (project.runtime !== 'typescript' && project.runtime !== 'protobuf') {
+      return;
+    }
+
+    let dir;
+    switch (project.type) {
+      case 'static':
+        dir = '';
+        break;
+
+      case 'library':
+        dir = 'libs';
+        break;
+
+      case 'service':
+        dir = 'services';
+        break;
+
+      default:
+        throw new Error(`Unsupported project type: ${project.type}`);
+    }
+
+    const projectPath = path.resolve(__dirname, '../../projects/', dir, projectName);
+
+    console.log(`* Building ${projectName}\n`.green);
+    shell.cd(projectPath);
+    const shellStringOutput = shell.exec('./build.sh');
+    if (shellStringOutput.code !== 0) {
+      throw new Error(`Error ${shellStringOutput.code} in ${projectPath}\n`.red);
     }
   });
 
   console.log(' * Done\n'.green);
 }
 
-main().catch(() => process.exit(1));
+main().catch((e) => {
+  console.error(e);
+
+  process.exit(1);
+});
--- build/src/watch-ts.js
+++ build/src/watch-ts.js
@@ -8,13 +8,40 @@ require('colors');
 async function main() {
   projects.order.forEach((projectName) => {
     const project = projects[projectName];
-    project.path = path.resolve(__dirname, '../../projects/', projectName);
-    if (project.runtime === 'typescript') {
-      console.log(` * Watching ${projectName}\n`.green);
-      shell.cd(project.path);
-      shell.exec('./watch.sh', { async: true });
+
+    if (project.runtime !== 'typescript') {
+      return;
     }
+
+    let dir;
+    switch (project.type) {
+      case 'static':
+        dir = '';
+        break;
+
+      case 'library':
+        dir = 'libs';
+        break;
+
+      case 'service':
+        dir = 'services';
+        break;
+
+      default:
+        throw new Error(`Unsupported project type: ${project.type}`);
+    }
+
+    const projectPath = path.resolve(__dirname, '../../projects/', dir, projectName);
+
+    console.log(` * Watching ${projectName}\n`.green);
+    shell.cd(projectPath);
+    shell.exec('./watch.sh', { async: true });
   });
 }
 
-main();
+main().catch((e) => {
+  console.error(e);
+
+  process.exit(1);
+});
+
