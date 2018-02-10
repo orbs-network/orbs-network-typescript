f42c9af34a967eb20358a58a90fbca113c65d65a
Leonid Beder <leonid@orbs.com>
2018-02-10 21:47:00 +0200

    Restore gossip config

--- config/topologies/discovery/node1/consensus.js
+++ config/topologies/discovery/node1/consensus.js
@@ -1,3 +1,5 @@
+const GOSSIP_PEERS = process.env.GOSSIP_PEERS ? process.env.GOSSIP_PEERS.split(',') : undefined;
+
 module.exports = {
   name: 'consensus',
   version: '1.0.0',
@@ -12,5 +14,14 @@ module.exports = {
       service: 'virtual-machine',
       endpoint: 'virtual-machine:51151',
     },
+  ],
+  gossipPort: 60001,
+  gossipPeers: GOSSIP_PEERS || [
+    'ws://172.1.1.2:60001',
+    'ws://172.1.1.3:60001',
+    'ws://172.1.1.4:60001',
+    'ws://172.1.1.5:60001',
+    'ws://172.1.1.6:60001',
+    'ws://172.1.1.7:60001',
   ],
 };
