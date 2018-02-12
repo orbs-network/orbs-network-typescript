93612d0568eee888def8d116966a9a1bd0733df0
Shai Yallin <shai.yallin@gmail.com>
2018-02-12 19:48:17 +0200

    added @types for sinon and chai-as-promised, fixed indentation

--- projects/libs/core-library-typescript/src/public-api/transaction-handler.ts
+++ projects/libs/core-library-typescript/src/public-api/transaction-handler.ts
@@ -1,30 +1,29 @@
 import { types } from "../common-library/types";
 
 export interface TransactionHandlerConfig {
-    validateSubscription(): boolean;
+  validateSubscription(): boolean;
 }
 
 export class TransactionHandler {
-    private consensus: types.ConsensusClient;
-    private config: TransactionHandlerConfig;
+  private consensus: types.ConsensusClient;
+  private config: TransactionHandlerConfig;
 
-    public async handle(transaction: types.SendTransactionInput) {
+  public async handle(transaction: types.SendTransactionInput) {
+    if (this.config.validateSubscription()) {
+      const subscriptionKey = transaction.transactionAppendix.subscriptionKey;
 
-        if (this.config.validateSubscription()) {
-            const subscriptionKey = transaction.transactionAppendix.subscriptionKey;
-            // console.log("sendTransaction", this.peers.subscriptionManager);
-            const { active } = await this.consensus.getSubscriptionStatus({ subscriptionKey });
+      const { active } = await this.consensus.getSubscriptionStatus({ subscriptionKey });
 
-            if (!active) {
-               throw new Error(`subscription with key [${subscriptionKey}] inactive`);
-            }
-        }
-
-        await this.consensus.sendTransaction(transaction);
+      if (!active) {
+        throw new Error(`subscription with key [${subscriptionKey}] inactive`);
+      }
     }
 
-    constructor(consensus: types.ConsensusClient, config: TransactionHandlerConfig) {
-        this.consensus = consensus;
-        this.config = config;
-    }
+    await this.consensus.sendTransaction(transaction);
+  }
+
+  constructor(consensus: types.ConsensusClient, config: TransactionHandlerConfig) {
+    this.consensus = consensus;
+    this.config = config;
+  }
 }
