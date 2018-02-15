8a10fc86120d950e9301f7145ba8066716b23c72
idandush <idanofrat@gmail.com>
2018-02-15 19:08:14 +0200

    removed temp mounting from docker test

--- docker-test.sh
+++ docker-test.sh
@@ -16,6 +16,6 @@ if [ $EXIT_CODE -ne 0 ] ; then
 fi
 
 docker network create public-api-external-network --subnet 172.2.2.0/24 || true
-docker run -ti --rm --privileged  --network=public-api-external-network --ip 172.2.2.9 -e PREEXISTING_PUBLIC_API_SUBNET=172.2.2 -e CONNECT_FROM_HOST=false -e DOCKER_IMAGE -e DOCKER_TAG -v /Users/idan/dev/orbs/orbs-network/e2e/src:/opt/orbs/e2e/src -v /var/run/docker.sock:/var/run/docker.sock $DOCKER_IMAGE:$DOCKER_TAG  bash -c 'apk add --no-cache docker py-pip && pip install docker-compose && cd e2e && ./build.sh && ./test.sh'
+docker run -ti --rm --privileged  --network=public-api-external-network --ip 172.2.2.9 -e PREEXISTING_PUBLIC_API_SUBNET=172.2.2 -e CONNECT_FROM_HOST=false -e DOCKER_IMAGE -e DOCKER_TAG -v /var/run/docker.sock:/var/run/docker.sock $DOCKER_IMAGE:$DOCKER_TAG  bash -c 'apk add --no-cache docker py-pip && pip install docker-compose && cd e2e && ./build.sh && ./test.sh'
 
 exit $EXIT_CODE
