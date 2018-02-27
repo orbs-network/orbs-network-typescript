9656a98983b68b687c595c4740239f9419b22ec3
Leonid Beder <leonid@orbs.com>
2018-02-27 20:19:32 +0200

    Decouple logger from config, specify its default config and allow its reconfiguration

--- projects/libs/core-library-typescript/src/common-library/logger.ts
+++ projects/libs/core-library-typescript/src/common-library/logger.ts
@@ -6,54 +6,63 @@ import * as path from "path";
 import * as fs from "fs";
 import { defaults } from "lodash";
 
-import { config } from "./config";
-
 const { NODE_NAME, NODE_IP, NODE_ENV, SERVICE_NAME } = process.env;
 
+export interface LogzIoOptions {
+  enabled: boolean;
+  apiKey: string;
+}
+
+export interface FileOptions {
+  fileName: string;
+  json?: boolean;
+  maxSize?: number;
+  maxFiles?: number;
+}
+
+export interface LoggerOptions {
+  level?: string;
+  file?: FileOptions;
+  console?: boolean;
+  logzio?: LogzIoOptions;
+}
+
 export class Logger {
-  public static readonly DEFAULT_OPTIONS = {
-    level: "info",
-    fileName: "logs/default.log",
-    maxSize: 10 * 1024 * 1024,
-    maxFiles: 20,
-    console: false,
-    json: true,
+  public static readonly LOG_TYPES = ["debug", "info", "warn", "error"];
+  public static readonly DEFAULT_LOG_LEVEL = "info";
+  private static readonly LOGZIO_HOST = "listener.logz.io";
+
+  public static readonly DEFAULT_OPTIONS: LoggerOptions = {
+    level: Logger.DEFAULT_LOG_LEVEL,
+    console: Logger.isTest(),
     logzio: {
       enabled: false,
       apiKey: ""
     }
   };
 
-  public static readonly LOG_TYPES = ["debug", "info", "warn", "error"];
-  public static readonly DEFAULT_LOG_LEVEL = "info";
+  public static readonly DEFAULT_FILE_OPTIONS: FileOptions = {
+    fileName: undefined,
+    maxSize: 10 * 1024 * 1024,
+    maxFiles: 20,
+    json: !Logger.isTest()
+  };
 
-  private static readonly CONFIG_LOG = "logger";
-  private static readonly LOGZIO_HOST = "listener.logz.io";
+  private logger: winston.LoggerInstance;
 
-  private _logger: winston.LoggerInstance;
+  public constructor() {
+    this.configure();
+  }
 
-  constructor(options = {}) {
-    const opts = defaults(options, Logger.DEFAULT_OPTIONS);
+  public configure(options?: LoggerOptions) {
+    const loggerOptions = defaults(options, Logger.DEFAULT_OPTIONS);
 
-    if (Logger.LOG_TYPES.indexOf(opts.level) == -1) {
-      opts.level = Logger.DEFAULT_LOG_LEVEL;
+    if (Logger.LOG_TYPES.indexOf(loggerOptions.level) == -1) {
+      loggerOptions.level = Logger.DEFAULT_LOG_LEVEL;
     }
 
-    // Create the directory, if it does not exist.
-    Logger.mkdir(opts.fileName);
-
-    this._logger = new winston.Logger({
-      level: opts.level,
-      transports: [
-        new winston.transports.File({
-          filename: Logger.resolvePath(opts.fileName),
-          maxsize: opts.maxSize,
-          maxFiles: opts.maxFiles,
-          prettyPrint: false,
-          tailable: true,
-          json: opts.json
-        })
-      ],
+    this.logger = new winston.Logger({
+      level: loggerOptions.level,
       rewriters: [
         (level, msg, meta) => {
           meta.node = NODE_NAME;
@@ -66,41 +75,46 @@ export class Logger {
       ]
     });
 
-    // Enable console output, during development.
-    if (opts.console) {
+    // Enable file output.
+    if (loggerOptions.file && loggerOptions.file.fileName) {
+      this.enableFileTransport(loggerOptions.file);
+    }
+
+    // Enable console output.
+    if (loggerOptions.console) {
       this.enableConsole();
     }
 
     // Enable log shipping to Logz.io (according to the configuration).
-    if (opts.logzio && opts.logzio.enabled) {
-      this.enableLogzio(opts.logzio.apiKey, opts.level);
+    if (loggerOptions.logzio && loggerOptions.logzio.enabled) {
+      this.enableLogzio(loggerOptions.level, loggerOptions.logzio.apiKey);
     }
   }
 
-  public static fromConfiguration(): Logger {
-    const loggerConfig = config.get(Logger.CONFIG_LOG);
-
-    return new Logger(loggerConfig);
-  }
-
   public readonly debug: winston.LeveledLogMethod = (msg: string, ...meta: any[]): winston.LoggerInstance => {
-    return this._logger.debug(msg, ...meta);
+    return this.logMessage(this.logger.debug, msg, ...meta);
   }
 
   public readonly info: winston.LeveledLogMethod = (msg: string, ...meta: any[]): winston.LoggerInstance => {
-    return this._logger.info(msg, ...meta);
+    return this.logMessage(this.logger.info, msg, ...meta);
   }
 
   public readonly warn: winston.LeveledLogMethod = (msg: string, ...meta: any[]): winston.LoggerInstance => {
-    return this._logger.warn(msg, ...meta);
+    return this.logMessage(this.logger.warn, msg, ...meta);
   }
 
   public readonly error: winston.LeveledLogMethod = (msg: string | Error, ...meta: any[]): winston.LoggerInstance => {
-    return this.logError(this._logger.error, msg, ...meta);
+    return this.logError(this.logger.error, msg, ...meta);
   }
 
   public readonly fatal: winston.LeveledLogMethod = (msg: string | Error, ...meta: any[]): winston.LoggerInstance => {
-    return this.logError(this._logger.emerg, msg, ...meta);
+    return this.logError(this.logger.emerg, msg, ...meta);
+  }
+
+  private logMessage(method: winston.LeveledLogMethod, msg: string, ...meta: any[]): winston.LoggerInstance {
+    method(<string>msg, ...meta);
+
+    return this.logger;
   }
 
   private logError(method: winston.LeveledLogMethod, msg: string | Error, ...meta: any[]): winston.LoggerInstance {
@@ -110,38 +124,56 @@ export class Logger {
       method(msg.stack, ...meta);
     }
 
-    return this._logger;
+    return this.logger;
   }
 
   private static mkdir(fileName: string): void {
     // Create the directory, if it does not exist.
-    const logsDir = Logger.resolvePath(path.dirname(fileName));
+    const logsDir = path.dirname(fileName);
     if (!fs.existsSync(logsDir)) {
       fs.mkdirSync(logsDir);
     }
   }
 
-  private static resolvePath(pathname: string): string {
-    return path.resolve(path.join(__dirname, "../../../../../", pathname));
+  private enableFileTransport(options: FileOptions): void {
+    const fileOptions = defaults(options, Logger.DEFAULT_FILE_OPTIONS);
+
+    fileOptions.fileName = path.resolve(fileOptions.fileName);
+
+    // Create the directory, if it does not exist.
+    Logger.mkdir(fileOptions.fileName);
+
+    this.logger.add(winston.transports.File, {
+      filename: fileOptions.fileName,
+      maxsize: fileOptions.maxSize,
+      maxFiles: fileOptions.maxFiles,
+      json: fileOptions.json,
+      prettyPrint: false,
+      tailable: true,
+    });
   }
 
   private enableConsole(): void {
-    this._logger.add(winston.transports.Console, {
+    this.logger.add(winston.transports.Console, {
       json: false,
       colorize: true,
       timestamp: true
     });
   }
 
-  private enableLogzio(apiKey: string, level: string): void {
+  private enableLogzio(level: string, apiKey: string): void {
     const winstonLogzioTransport = require("winston-logzio");
 
-    this._logger.add(winstonLogzioTransport, {
+    this.logger.add(winstonLogzioTransport, {
       level: level,
       token: apiKey,
       host: Logger.LOGZIO_HOST
     });
   }
+
+  private static isTest(): boolean {
+    return process.env.NODE_ENV === "test";
+  }
 }
 
-export const logger = Logger.fromConfiguration();
+export const logger = new Logger();
