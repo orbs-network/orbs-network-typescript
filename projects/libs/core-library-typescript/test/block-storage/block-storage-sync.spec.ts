889034ff0cc3bbd6556608ec5832fa9b9a5c70ed
Kirill Maksimov <kirill@orbs.network>
2018-02-27 16:40:01 +0200

    Fix test indent

--- projects/libs/core-library-typescript/test/block-storage/block-storage-sync.spec.ts
+++ projects/libs/core-library-typescript/test/block-storage/block-storage-sync.spec.ts
@@ -7,103 +7,103 @@ import * as sinonChai from "sinon-chai";
 import * as fsExtra from "fs-extra";
 
 async function init(): Promise<any> {
-    const blockStorage = new BlockStorage();
-    await blockStorage.load();
-    const blockStorageSync = new BlockStorageSync(blockStorage);
-    return { blockStorage, blockStorageSync };
+  const blockStorage = new BlockStorage();
+  await blockStorage.load();
+  const blockStorageSync = new BlockStorageSync(blockStorage);
+  return { blockStorage, blockStorageSync };
 }
 
 function generateBlock(prevBlockId: number): types.Block {
-    return {
-        header: {
-            version: 0,
-            id: prevBlockId + 1,
-            prevBlockId: prevBlockId
-        },
-        tx: { contractAddress: "0", sender: "", signature: "", payload: "{}" },
-        modifiedAddressesJson: "{}"
-    };
+  return {
+    header: {
+      version: 0,
+      id: prevBlockId + 1,
+      prevBlockId: prevBlockId
+    },
+    tx: { contractAddress: "0", sender: "", signature: "", payload: "{}" },
+    modifiedAddressesJson: "{}"
+  };
 }
 
 describe("Block storage sync", () => {
-    let blockStorage: BlockStorage;
-    let blockStorageSync: BlockStorageSync;
+  let blockStorage: BlockStorage;
+  let blockStorageSync: BlockStorageSync;
 
-    beforeEach(async () => {
-        try {
-            fsExtra.removeSync(BlockStorage.LEVELDB_PATH);
-        } catch (e) { }
+  beforeEach(async () => {
+    try {
+        fsExtra.removeSync(BlockStorage.LEVELDB_PATH);
+    } catch (e) { }
 
-        const results = await init();
-        blockStorage = results.blockStorage;
-        blockStorageSync = results.blockStorageSync;
+    const results = await init();
+    blockStorage = results.blockStorage;
+    blockStorageSync = results.blockStorageSync;
+  });
+
+  afterEach(async () => {
+    await blockStorage.shutdown();
+    blockStorage = undefined;
+    blockStorageSync = undefined;
+  });
+
+  describe("queue", () => {
+    it("accepts new blocks", () => {
+      blockStorageSync.getQueueSize().should.be.eql(0);
+
+      blockStorageSync.onReceiveBlock(generateBlock(0));
+
+      blockStorageSync.getQueueSize().should.be.eql(1);
+    });
+  });
+
+  describe("#appendBlocks", () => {
+    it("does not care about order of the blocks", async () => {
+      const blocks = [
+        generateBlock(0),
+        generateBlock(1),
+        generateBlock(2),
+        generateBlock(3)
+      ];
+
+      blockStorageSync.onReceiveBlock(blocks[1]);
+      blockStorageSync.onReceiveBlock(blocks[2]);
+      blockStorageSync.onReceiveBlock(blocks[0]);
+      blockStorageSync.onReceiveBlock(blocks[3]);
+
+      blockStorageSync.getQueueSize().should.be.eql(4);
+      await blockStorageSync.appendBlocks();
+      blockStorageSync.getQueueSize().should.be.eql(0);
+
+      await blockStorage.getBlocks(0).should.eventually.be.eql(blocks);
+      await blockStorage.getBlocks(4).should.eventually.be.empty;
     });
 
-    afterEach(async () => {
-        await blockStorage.shutdown();
-        blockStorage = undefined;
-        blockStorageSync = undefined;
+    it("does not return blocks back to the queue in case something happends", async () => {
+      const blocks = [
+        generateBlock(0),
+        generateBlock(1),
+        generateBlock(2),
+        generateBlock(3),
+        generateBlock(5)
+      ];
+
+      blockStorageSync.onReceiveBlock(blocks[1]);
+      blockStorageSync.onReceiveBlock(blocks[2]);
+      blockStorageSync.onReceiveBlock(blocks[0]);
+      blockStorageSync.onReceiveBlock(blocks[4]);
+      blockStorageSync.onReceiveBlock(blocks[3]);
+
+      blockStorageSync.getQueueSize().should.be.eql(5);
+
+      const appendBlocksPromise = blockStorageSync.appendBlocks();
+      try {
+        await appendBlocksPromise;
+      } catch (e) { }
+
+      appendBlocksPromise.should.eventually.be.rejected;
+      blockStorageSync.getQueueSize().should.be.eql(0);
+
+      await blockStorage.getBlocks(0).should.eventually.be.eql(blocks.slice(0, 4));
+      await blockStorage.getBlocks(4).should.eventually.be.empty;
     });
-
-    describe("queue", () => {
-        it("accepts new blocks", () => {
-            blockStorageSync.getQueueSize().should.be.eql(0);
-
-            blockStorageSync.onReceiveBlock(generateBlock(0));
-
-            blockStorageSync.getQueueSize().should.be.eql(1);
-        });
-    });
-
-    describe("#appendBlocks", () => {
-        it("does not care about order of the blocks", async () => {
-            const blocks = [
-                generateBlock(0),
-                generateBlock(1),
-                generateBlock(2),
-                generateBlock(3)
-            ];
-
-            blockStorageSync.onReceiveBlock(blocks[1]);
-            blockStorageSync.onReceiveBlock(blocks[2]);
-            blockStorageSync.onReceiveBlock(blocks[0]);
-            blockStorageSync.onReceiveBlock(blocks[3]);
-
-            blockStorageSync.getQueueSize().should.be.eql(4);
-            await blockStorageSync.appendBlocks();
-            blockStorageSync.getQueueSize().should.be.eql(0);
-
-            await blockStorage.getBlocks(0).should.eventually.be.eql(blocks);
-            await blockStorage.getBlocks(4).should.eventually.be.empty;
-        });
-
-        it("does not return blocks back to the queue in case something happends", async () => {
-            const blocks = [
-                generateBlock(0),
-                generateBlock(1),
-                generateBlock(2),
-                generateBlock(3),
-                generateBlock(5)
-            ];
-
-            blockStorageSync.onReceiveBlock(blocks[1]);
-            blockStorageSync.onReceiveBlock(blocks[2]);
-            blockStorageSync.onReceiveBlock(blocks[0]);
-            blockStorageSync.onReceiveBlock(blocks[4]);
-            blockStorageSync.onReceiveBlock(blocks[3]);
-
-            blockStorageSync.getQueueSize().should.be.eql(5);
-
-            const appendBlocksPromise = blockStorageSync.appendBlocks();
-            try {
-                await appendBlocksPromise;
-            } catch (e) { }
-
-            appendBlocksPromise.should.eventually.be.rejected;
-            blockStorageSync.getQueueSize().should.be.eql(0);
-
-            await blockStorage.getBlocks(0).should.eventually.be.eql(blocks.slice(0, 4));
-            await blockStorage.getBlocks(4).should.eventually.be.empty;
-        });
-    });
+  });
 });
