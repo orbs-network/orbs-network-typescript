8bbd8cd5df47a188f447143eeaca0d671dbfce35
Leonid Beder <leonid@orbs.com>
2018-03-05 11:45:33 +0200

    Make sure that libsodium is properly linked

--- client/crypto-sdk/lib/account.cpp
+++ client/crypto-sdk/lib/account.cpp
@@ -1,9 +1,10 @@
 #include "account.h"
 
+#include <sodium.h>
+
 using namespace Orbs;
 
-Account::Account()
-{
+Account::Account() {
 }
 
 Account::~Account() {
--- client/crypto-sdk/test/account-test.cpp
+++ client/crypto-sdk/test/account-test.cpp
@@ -1,5 +1,11 @@
 #include "gtest/gtest.h"
 
+#include "../lib/account.h"
+
+using namespace Orbs;
+
 TEST(sample_test_case, sample_test) {
+    Orbs::Account a;
+
     EXPECT_EQ(1, 1);
 }
