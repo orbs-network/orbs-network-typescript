f4e7511a44fa977a840b9707ba524219f261d54a
Leonid Beder <leonid@orbs.com>
2018-03-06 23:01:05 +0200

    Fix indent

--- projects/services/storage-service-typescript/test/fake-gossip-client.ts
+++ projects/services/storage-service-typescript/test/fake-gossip-client.ts
@@ -2,42 +2,42 @@
 import { types } from "orbs-core-library";
 
 export default class FakeGossipClient implements types.GossipClient {
-    nodes = new Map<string, types.ClientMap>();
-    nodeName: string;
+  nodes = new Map<string, types.ClientMap>();
+  nodeName: string;
 
-    constructor(nodeName: string) {
-      this.nodeName = nodeName;
+  constructor(nodeName: string) {
+    this.nodeName = nodeName;
+  }
+
+  broadcastMessage(input: types.BroadcastMessageInput): types.BroadcastMessageOutput {
+    for (const nodeName of this.nodes.keys()) {
+      this.unicastMessage({
+        recipient: nodeName,
+        broadcastGroup: input.broadcastGroup,
+        buffer: input.buffer,
+        messageType: input.messageType,
+        immediate: input.immediate
+      });
     }
+    return {};
+  }
 
-    broadcastMessage(input: types.BroadcastMessageInput): types.BroadcastMessageOutput {
-      for (const nodeName of this.nodes.keys()) {
-        this.unicastMessage({
-          recipient: nodeName,
-          broadcastGroup: input.broadcastGroup,
-          buffer: input.buffer,
-          messageType: input.messageType,
-          immediate: input.immediate
-        });
-      }
+  unicastMessage(input: types.UnicastMessageInput): types.UnicastMessageOutput {
+    const clientMap = <any>this.nodes.get(input.recipient);
+    if (clientMap) {
+      clientMap[input.broadcastGroup].gossipMessageReceived({
+        fromAddress: this.nodeName,
+        broadcastGroup: input.broadcastGroup,
+        messageType: input.messageType,
+        buffer: input.buffer
+      });
       return {};
-    }
-
-    unicastMessage(input: types.UnicastMessageInput): types.UnicastMessageOutput {
-      const clientMap = <any> this.nodes.get(input.recipient);
-      if (clientMap) {
-        clientMap[input.broadcastGroup].gossipMessageReceived({
-          fromAddress: this.nodeName,
-          broadcastGroup: input.broadcastGroup,
-          messageType: input.messageType,
-          buffer: input.buffer
-        });
-        return {};
-      } else {
-        throw new Error(`could not find recipient node ${input.recipient}`);
-      }
-    }
-
-    addNode(nodeName: string, clientMap: types.ClientMap) {
-      this.nodes.set(nodeName, clientMap);
+    } else {
+      throw new Error(`could not find recipient node ${input.recipient}`);
     }
   }
+
+  addNode(nodeName: string, clientMap: types.ClientMap) {
+    this.nodes.set(nodeName, clientMap);
+  }
+}
--- projects/services/storage-service-typescript/test/service-ipc-client.ts
+++ projects/services/storage-service-typescript/test/service-ipc-client.ts
@@ -1,18 +1,18 @@
 export function generateServiceIPCClient<T>(service: any): T {
-    const client = {};
+  const client = {};
 
-    for (const propertyName of Object.getOwnPropertyNames(Object.getPrototypeOf(service))) {
+  for (const propertyName of Object.getOwnPropertyNames(Object.getPrototypeOf(service))) {
 
-      Object.defineProperty(client, propertyName, {
-        value: async function(req: any) {
-          const ctx = {req, res: {}};
-          await service[propertyName](ctx);
+    Object.defineProperty(client, propertyName, {
+      value: async function (req: any) {
+        const ctx = { req, res: {} };
+        await service[propertyName](ctx);
         //   console.log(`called ${propertyName} with ${JSON.stringify(req)}. result: ${JSON.stringify(ctx.res)}`);
-          return ctx.res;
-        },
-        enumerable: true
-      });
-    }
-
-    return <T>client;
+        return ctx.res;
+      },
+      enumerable: true
+    });
   }
+
+  return <T>client;
+}
