62b09d8effd6184dc506abc20c5045ebc3d6f1fb
Leonid Beder <leonid@orbs.com>
2018-03-06 17:40:34 +0200

    Add init code

--- client/crypto-sdk/test/main.cpp
+++ client/crypto-sdk/test/main.cpp
@@ -1,9 +1,16 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
+#include "../lib/crypto.h"
+
+using namespace testing;
+using namespace Orbs;
+
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    ::testing::InitGoogleMock(&argc, argv);
+    CryptoSDK::Init();
+
+    InitGoogleTest(&argc, argv);
+    InitGoogleMock(&argc, argv);
 
     return RUN_ALL_TESTS();
 }
