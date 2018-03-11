198b9d5f92d3dd4d6089acd4d7e511e59fd18446
Leonid Beder <leonid@orbs.com>
2018-03-11 11:01:37 +0200

    Include version

--- client/crypto-sdk/libgpg-error.cmake
+++ client/crypto-sdk/libgpg-error.cmake
@@ -4,7 +4,7 @@ set(LIBGPG_ERROR_VERSION 1.27)
 
 include(ExternalProject)
 ExternalProject_Add(libgpg-error
-  URL "https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.27.tar.bz2"
+  URL "https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-${LIBGPG_ERROR_VERSION}.tar.bz2"
   URL_HASH SHA256=4f93aac6fecb7da2b92871bb9ee33032be6a87b174f54abf8ddf0911a22d29d2
   CONFIGURE_COMMAND <SOURCE_DIR>/configure
     --host=${TARGET_ARCH}
