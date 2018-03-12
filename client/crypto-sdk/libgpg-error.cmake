9e15caa95c43657d377bb8fb844601400f8732f8
Leonid Beder <leonid@orbs.com>
2018-03-12 08:23:18 +0200

    Properly register libgpg-error

--- client/crypto-sdk/libgpg-error.cmake
+++ client/crypto-sdk/libgpg-error.cmake
@@ -22,3 +22,25 @@ ExternalProject_Add(libgpg-error
   LOG_BUILD ON
   LOG_INSTALL ON
 )
+
+ExternalProject_Get_Property(libgpg-error BINARY_DIR)
+set(LIBGPG_ERROR_INCLUDE_DIRS ${BINARY_DIR}/src/)
+
+ExternalProject_Add_Step(libgpg-error copy-gpg-error-config
+  DEPENDEES install
+  COMMAND mkdir -p ${BINARY_DIR}/src/bin && cp ${BINARY_DIR}/src/gpg-error-config ${BINARY_DIR}/src/bin
+)
+
+# The cloning of the above repo doesn't happen until make, however if the dir doesn't exist,
+# INTERFACE_INCLUDE_DIRECTORIES will throw an error. To make it work, we just create the directory now during config.
+file(MAKE_DIRECTORY ${LIBGPG_ERROR_INCLUDE_DIRS})
+
+set(LIBGPG_ERROR_LIBRARY_PATH ${BINARY_DIR}/src/.libs/libgpg-error.a)
+set(LIBGPG_ERROR_LIBRARY gpg-error)
+add_library(${LIBGPG_ERROR_LIBRARY} UNKNOWN IMPORTED)
+set_target_properties(${LIBGPG_ERROR_LIBRARY} PROPERTIES
+  "IMPORTED_LOCATION" "${LIBGPG_ERROR_LIBRARY_PATH}"
+  "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
+  "INTERFACE_INCLUDE_DIRECTORIES" "${LIBGPG_ERROR_INCLUDE_DIRS}")
+add_dependencies(${LIBGPG_ERROR_LIBRARY} libgpg-error)
+
