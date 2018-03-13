30242a2f2e9036988af22f54672af1009c78ef67
Kirill Maksimov <kirill@orbs.network>
2018-03-13 20:26:56 +0200

    Disable subscription validation by default

--- projects/services/public-api-service-typescript/src/index.ts
+++ projects/services/public-api-service-typescript/src/index.ts
@@ -4,7 +4,7 @@ import { ErrorHandler, grpc, ServiceRunner, topology, topologyPeers, logger } fr
 
 import PublicApiService from "./service";
 
-const { NODE_NAME, LOGZIO_API_KEY } = process.env;
+const { NODE_NAME, LOGZIO_API_KEY, VALIDATE_SUBSCRIPTION } = process.env;
 
 ErrorHandler.setup();
 
@@ -24,6 +24,10 @@ if (!NODE_NAME) {
 
 const nodeTopology = topology();
 const peers = topologyPeers(nodeTopology.peers);
-const nodeConfig = { nodeName: NODE_NAME };
+const validateSubscription = VALIDATE_SUBSCRIPTION === "true";
+const nodeConfig = {
+  nodeName: NODE_NAME,
+  validateSubscription
+};
 
 ServiceRunner.run(grpc.publicApiServer, new PublicApiService(peers.virtualMachine, peers.transactionPool, peers.subscriptionManager, nodeConfig), nodeTopology.endpoint);
