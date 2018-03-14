6ec79ae3d1d6f8b4b7680ecd963ae947c145bd4b
Leonid Beder <leonid@orbs.com>
2018-03-14 08:08:05 +0200

    Typo

--- client/crypto-sdk/lib/crypto.cpp
+++ client/crypto-sdk/lib/crypto.cpp
@@ -19,24 +19,24 @@ void CryptoSDK::Init() {
     // suppress such warnings.
     gcry_error_t err;
     if ((err = gcry_control(GCRYCTL_SUSPEND_SECMEM_WARN))) {
-        throw runtime_error("Failed to initialize GCRYCTL_SUSPEND_SECMEM_WARN with: " + to_string(err));
+        throw runtime_error("Failed to send GCRYCTL_SUSPEND_SECMEM_WARN with: " + to_string(err));
     }
 
     // Allocate a pool of 16k secure memory. This makes the secure memory available and also drops privileges where
     // needed. Note that by using functions like gcry_xmalloc_secure and gcry_mpi_snew Libgcrypt may expand the secure
     // memory pool with memory which lacks the property of not being swapped out to disk.
     if ((err = gcry_control(GCRYCTL_INIT_SECMEM, SECMEM_SIZE, 0))) {
-        throw runtime_error("Failed to initialize GCRYCTL_INIT_SECMEM with: " + to_string(err));
+        throw runtime_error("Failed to send GCRYCTL_INIT_SECMEM with: " + to_string(err));
     }
 
     // It is now okay to let Libgcrypt complain when there was/is a problem with the secure memory.
     if ((err = gcry_control(GCRYCTL_RESUME_SECMEM_WARN))) {
-        throw runtime_error("Failed to initialize GCRYCTL_RESUME_SECMEM_WARN with: " + to_string(err));
+        throw runtime_error("Failed to send GCRYCTL_RESUME_SECMEM_WARN with: " + to_string(err));
     }
 
     // Tell Libgcrypt that initialization has completed.
     if ((err = gcry_control(GCRYCTL_INITIALIZATION_FINISHED, 0))) {
-        throw runtime_error("Failed to initialize GCRYCTL_INITIALIZATION_FINISHED with: " + to_string(err));
+        throw runtime_error("Failed to send GCRYCTL_INITIALIZATION_FINISHED with: " + to_string(err));
     }
 }
 
@@ -44,7 +44,7 @@ void CryptoSDK::Init() {
 void CryptoSDK::InitFIPSMode() {
     gcry_error_t err;
     if ((err = gcry_control(GCRYCTL_FORCE_FIPS_MODE))) {
-        throw runtime_error("Failed to initialize GCRYCTL_FORCE_FIPS_MODE with: " + to_string(err));
+        throw runtime_error("Failed to send GCRYCTL_FORCE_FIPS_MODE with: " + to_string(err));
     }
 
     Init();
