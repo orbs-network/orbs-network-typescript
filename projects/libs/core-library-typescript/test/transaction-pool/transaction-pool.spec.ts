ab0efead129e5eb00ab0b4a0457315faf7fb5fcf
Kirill Maksimov <kirill@orbs.com>
2018-03-18 15:18:34 +0200

    Add test for clearing transactions through Gossip

--- projects/libs/core-library-typescript/test/transaction-pool/transaction-pool.spec.ts
+++ projects/libs/core-library-typescript/test/transaction-pool/transaction-pool.spec.ts
@@ -5,12 +5,11 @@ import * as chaiAsPromised from "chai-as-promised";
 import * as sinonChai from "sinon-chai";
 import { stubInterface } from "ts-sinon";
 import { TransactionPool } from "../../src/transaction-pool";
+import { GossipClient } from "orbs-interfaces";
 
 chai.should();
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
-
-const gossip = stubInterface<types.GossipClient>();
 
 function aTransaction() {
   const transaction: types.Transaction = {
@@ -24,10 +23,12 @@ function aTransaction() {
   return transaction;
 }
 
-describe("new broadcast transaction", () => {
-  let transactionPool;
+describe.only("new broadcast transaction", () => {
+  let transactionPool: TransactionPool;
+  let gossip: GossipClient;
 
   beforeEach(() => {
+    gossip = stubInterface<types.GossipClient>();
     transactionPool = new TransactionPool(gossip);
   });
 
@@ -46,5 +47,29 @@ describe("new broadcast transaction", () => {
     await transactionPool.addNewPendingTransaction(tx);
 
     expect(transactionPool.addNewPendingTransaction(tx)).to.eventually.be.rejectedWith("Transaction with hash 4dadf2569cefcb0b30c5e02bbf2ab226b0a2f564c6d14f488688e3468254ec43 already exists in the pool");
+  });
+
+  it("should propagate rejected transactions", async () => {
+    const tx = aTransaction();
+    await transactionPool.addNewPendingTransaction(tx);
+
+    expect(gossip.broadcastMessage).to.have.been.calledOnce;
+
+    await transactionPool.clearPendingTransactions([tx]);
+
+    expect(gossip.broadcastMessage).to.have.been.calledTwice;
+  });
+
+  it("should clear pending transaction upon receiveing gossip message", async () => {
+    const tx = aTransaction();
+    await transactionPool.addNewPendingTransaction(tx);
+
+    const transactionsBefore = (await transactionPool.getAllPendingTransactions()).transactions;
+    expect(transactionsBefore).to.be.eql([tx]);
+
+    await transactionPool.gossipMessageReceived("someNode", "clearPendingTransaction", { transaction: tx });
+
+    const transactionsAfter = (await transactionPool.getAllPendingTransactions()).transactions;
+    expect(transactionsAfter).to.be.empty;
   });
 });
