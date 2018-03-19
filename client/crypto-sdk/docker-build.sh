c47a569384257a5e754b634c01671d7549a9f019
Leonid Beder <leonid@orbs.com>
2018-03-19 13:51:23 +0200

    Remove docker build

--- client/crypto-sdk/docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-export DOCKER_IMAGE=${DOCKER_IMAGE-orbs-crypto-sdk}
-export DOCKER_TAG=${DOCKER_TAG-$(git rev-parse --abbrev-ref HEAD | sed -e 's/\//-/g')}
-
-docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} .
