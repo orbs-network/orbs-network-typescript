f1f4aef540d3acf906cf6709ff1ace045b6d3119
Leonid Beder <leonid@orbs.com>
2018-03-20 20:21:40 +0200

    Implement the improved Orbs address scheme

--- client/crypto-sdk/lib/address.h
+++ client/crypto-sdk/lib/address.h
@@ -8,24 +8,35 @@ namespace Orbs {
 
 class Address {
 public:
+    static const uint8_t MAIN_NETWORK_ID;
+    static const uint8_t TEST_NETWORK_ID;
+
     static const uint8_t VERSION;
     static const uint32_t PUBLIC_KEY_SIZE;
-    static const uint32_t VERSION_FIELD_SIZE;
+    static const uint32_t NETWORK_ID_SIZE;
+    static const uint32_t VERSION_SIZE;
+    static const uint32_t VIRTUAL_CHAIN_ID_SIZE;
+    static const uint8_t VIRTUAL_CHAIN_ID_MSB;
     static const uint32_t ACCOUNT_ID_SIZE;
     static const uint32_t CHECKSUM_SIZE;
     static const uint32_t ADDRESS_LENGTH;
 
-    Address(const std::vector<uint8_t> &publicKey);
-    Address(const std::string &publicKey);
+    Address(const std::vector<uint8_t> &publicKey, const std::vector<uint8_t> &virtualChainId, uint8_t networkId);
+    Address(const std::string &publicKey, const std::string &virtualChainId, const std::string &networkId);
     virtual ~Address();
 
     const std::vector<uint8_t> &GetPublicKey() const;
+    uint8_t GetNetworkId() const;
     uint8_t GetVersion() const;
+    const std::vector<uint8_t> &GetVirtualChainId() const;
     const std::vector<uint8_t> &GetAccountId() const;
     const std::vector<uint8_t> &GetChecksum() const;
 
     const std::string ToString() const;
 
+    static bool IsValidNetworkId(uint8_t networkId);
+    static bool IsValidVersion(uint8_t version);
+    static bool IsValidVirtualChainId(const std::vector<uint8_t> &virtualChainId);
     static bool IsValid(const std::string &address);
 
 private:
@@ -33,6 +44,8 @@ private:
 
 private:
     std::vector<uint8_t> publicKey_;
+    std::vector<uint8_t> virtualChainId_;
+    uint8_t networkId_;
     uint8_t version_;
     std::vector<uint8_t> accountId_;
     std::vector<uint8_t> checksum_;
