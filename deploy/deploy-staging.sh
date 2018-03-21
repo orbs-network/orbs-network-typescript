4974b9ff59385828d1709a838d60d25c1989e3c2
Kirill Maksimov <kirill@orbs.com>
2018-03-21 18:20:21 +0200

    Speed up deployment (WIP)

--- deploy/deploy-staging.sh
+++ deploy/deploy-staging.sh
@@ -4,7 +4,7 @@ export NETWORK=testnet
 export AWS_ACCOUNT_ID=506367651493
 export DNS_ZONE=orbs-test.com
 export S3_BUCKET_NAME=orbs-network-config
-export REGIONS=${REGIONS-us-east-1 eu-central-1 ap-northeast-1 ap-northeast-2 ap-southeast-2 ca-central-1}
+export REGIONS=${REGIONS-us-east-1,eu-central-1,ap-northeast-1,ap-northeast-2,ap-southeast-2,ca-central-1}
 
 for REGION in $REGIONS
 do
