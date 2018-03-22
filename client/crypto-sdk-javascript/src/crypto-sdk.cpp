3b814dd8a0a79e02e58d9b195044230a08e9bd00
Leonid Beder <leonid@orbs.com>
2018-03-22 11:53:39 +0200

    Make sure to initialize the CryptoSDK

--- client/crypto-sdk-javascript/src/crypto-sdk.cpp
+++ client/crypto-sdk-javascript/src/crypto-sdk.cpp
@@ -1,6 +1,10 @@
 #include "address.h"
 
+#include "crypto-sdk/lib/crypto.h"
+
 napi_value Init(napi_env env, napi_value exports) {
+    Orbs::CryptoSDK::Init();
+
     return Address::Init(env, exports);
 }
 
