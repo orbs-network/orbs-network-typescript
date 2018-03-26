ae6c760206bf2787fdd11d90b1db65379a1fd96f
Kirill Maksimov <kirill@orbs.com>
2018-03-26 12:44:18 +0300

    Update deploy script

--- deploy/deploy-staging.sh
+++ deploy/deploy-staging.sh
@@ -5,27 +5,20 @@ export AWS_ACCOUNT_ID=506367651493
 export DNS_ZONE=orbs-test.com
 export S3_BUCKET_NAME=orbs-network-config
 export REGIONS=${REGIONS-us-east-1,eu-central-1,ap-northeast-1,ap-northeast-2,ap-southeast-2,ca-central-1}
+export DOCKER_TAG=${DOCKER_TAG-master}
+export DEPLOY_STEP=${DEPLOY_STEP-2}
 
-for REGION in $REGIONS
-do
-    echo $REGION
+yarn build-ts
 
-    # node src/deploy.js \
-    #     --region $REGION \
-    #     --dns-zone $DNS_ZONE \
-    #     --account-id $AWS_ACCOUNT_ID \
-    #     --network $NETWORK \
-    #     --s3-bucket-name $S3_BUCKET_NAME \
-    #     --create-basic-infrastructure
-
-    node src/deploy.js \
-        --region $REGION \
-        --dns-zone $DNS_ZONE \
-        --account-id $AWS_ACCOUNT_ID \
-        --network $NETWORK \
-        --s3-bucket-name $S3_BUCKET_NAME \
-        --tag-docker-image \
-        --push-docker-image \
-        --remove-node \
-        --deploy-node
-done
+node dist/deploy.js \
+    --region $REGIONS \
+    --dns-zone $DNS_ZONE \
+    --account-id $AWS_ACCOUNT_ID \
+    --network $NETWORK \
+    --s3-bucket-name $S3_BUCKET_NAME \
+    --docker-tag ${DOCKER_TAG} \
+    --step $DEPLOY_STEP \
+    --tag-docker-image \
+    --push-docker-image \
+    --remove-node \
+    --deploy-node
