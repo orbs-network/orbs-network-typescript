3729537df110f1b0a279c7253ddec33e1075353d
Leonid Beder <leonid@orbs.com>
2018-03-26 19:47:20 +0300

    Style

--- client/crypto-sdk/gtest.cmake
+++ client/crypto-sdk/gtest.cmake
@@ -3,8 +3,7 @@ find_package(Threads REQUIRED)
 set(GTEST_VERSION release-1.8.0)
 
 include(ExternalProject)
-ExternalProject_Add(
-  googletest
+ExternalProject_Add(googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
   GIT_TAG ${GTEST_VERSION}
   UPDATE_COMMAND ""
