302432f424803227768286bd71d7c6eaff727ced
Kirill Maksimov <kirill@orbs.com>
2018-03-28 14:56:25 +0300

    Minor deploy bugfixes, import parity node ip from basic infrastructure

--- deploy/deploy-staging.sh
+++ deploy/deploy-staging.sh
@@ -7,6 +7,7 @@ export S3_BUCKET_NAME=orbs-network-config
 export REGIONS=${REGIONS-us-east-1,eu-central-1,ap-northeast-1,ap-northeast-2,ap-southeast-2,ca-central-1}
 export DOCKER_TAG=${DOCKER_TAG-master}
 export DEPLOY_STEP=${DEPLOY_STEP-2}
+export ETHEREUM_NODE_IP=http://ethereum.services.orbs-test.com:8545
 
 npm run build-ts
 
@@ -18,6 +19,7 @@ node dist/deploy.js \
     --s3-bucket-name $S3_BUCKET_NAME \
     --docker-tag ${DOCKER_TAG} \
     --step $DEPLOY_STEP \
+    --ethereum-node-ip  \
     --tag-docker-image \
     --push-docker-image \
     --remove-node \
