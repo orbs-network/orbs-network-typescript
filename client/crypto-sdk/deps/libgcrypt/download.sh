6de3e9af74c19ef2a905a269fe15540fd4131322
Leonid Beder <leonid@orbs.com>
2018-03-28 14:35:15 +0300

    Style

--- client/crypto-sdk/deps/libgcrypt/download.sh
+++ client/crypto-sdk/deps/libgcrypt/download.sh
@@ -6,7 +6,7 @@ LIBGCRYPT_PACKAGE_FILENAME="${LIBGCRYPT_PACKAGE}.tar.bz2"
 LIBGCRYPT_SHA256="c8064cae7558144b13ef0eb87093412380efa16c4ee30ad12ecb54886a524c07"
 
 # Download the package.
-if [ ! -e ${LIBGCRYPT_PACKAGE_FILENAME} ]; then
+if [ ! -e ${LIBGCRYPT_PACKAGE_FILENAME} ] ; then
 	echo "Downloading ${LIBGCRYPT_PACKAGE_FILENAME}"
     curl -O https://www.gnupg.org/ftp/gcrypt/libgcrypt/${LIBGCRYPT_PACKAGE_FILENAME}
 else
@@ -15,7 +15,7 @@ fi
 
 # Verify the SHA256 of the package.
 echo "${LIBGCRYPT_SHA256}  ${LIBGCRYPT_PACKAGE_FILENAME}" | shasum -a256 -c
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
   echo "${LIBGCRYPT_PACKAGE_FILENAME} verification has failed!"
   exit 1
 else
--- client/crypto-sdk/deps/libgpg-error/download.sh
+++ client/crypto-sdk/deps/libgpg-error/download.sh
@@ -6,7 +6,7 @@ LIBGPG_ERROR_PACKAGE_FILENAME="${LIBGPG_ERROR_PACKAGE}.tar.bz2"
 LIBGPG_ERROR_SHA256="4f93aac6fecb7da2b92871bb9ee33032be6a87b174f54abf8ddf0911a22d29d2"
 
 # Download the package.
-if [ ! -e ${LIBGPG_ERROR_PACKAGE_FILENAME} ]; then
+if [ ! -e ${LIBGPG_ERROR_PACKAGE_FILENAME} ] ; then
 	echo "Downloading ${LIBGPG_ERROR_PACKAGE_FILENAME}"
     curl -O https://www.gnupg.org/ftp/gcrypt/libgpg-error/${LIBGPG_ERROR_PACKAGE_FILENAME}
 else
@@ -15,7 +15,7 @@ fi
 
 # Verify the SHA256 of the package.
 echo "${LIBGPG_ERROR_SHA256}  ${LIBGPG_ERROR_PACKAGE_FILENAME}" | shasum -a256 -c
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
   echo "${LIBGPG_ERROR_PACKAGE_FILENAME} verification has failed!"
   exit 1
 else
