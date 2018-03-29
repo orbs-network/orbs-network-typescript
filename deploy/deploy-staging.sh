406fb538af7598cc089cd922f59fea1f41a6da58
Kirill Maksimov <kirill@orbs.com>
2018-03-29 11:32:36 +0300

    Fix staging deploy

--- deploy/deploy-staging.sh
+++ deploy/deploy-staging.sh
@@ -7,7 +7,7 @@ export S3_BUCKET_NAME=orbs-network-config
 export REGIONS=${REGIONS-us-east-1,eu-central-1,ap-northeast-1,ap-northeast-2,ap-southeast-2,ca-central-1}
 export DOCKER_TAG=${DOCKER_TAG-master}
 export DEPLOY_STEP=${DEPLOY_STEP-2}
-export ETHEREUM_NODE_IP=http://ethereum.services.orbs-test.com:8545
+export ETHEREUM_NODE_IP=ethereum.services.orbs-test.com
 
 npm run build-ts
 
@@ -19,7 +19,7 @@ node dist/deploy.js \
     --s3-bucket-name $S3_BUCKET_NAME \
     --docker-tag ${DOCKER_TAG} \
     --step $DEPLOY_STEP \
-    --ethereum-node-ip  \
+    --ethereum-node-ip $ETHEREUM_NODE_IP \
     --tag-docker-image \
     --push-docker-image \
     --remove-node \
