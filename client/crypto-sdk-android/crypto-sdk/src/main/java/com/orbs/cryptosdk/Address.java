daf1b0d492f77b91e87ba6b5e3eb574b6875f24c
Leonid Beder <leonid@orbs.com>
2018-04-01 09:10:24 +0300

    Rename module

---
+++ client/crypto-sdk-android/crypto-sdk/src/main/java/com/orbs/cryptosdk/Address.java
@@ -0,0 +1,11 @@
+package com.orbs.cryptosdk;
+
+public class Address {
+    public Address(String publicKey, String virtualChainId, String networkId) {
+
+    }
+
+    public String toString() {
+        return "M1HelloWorld!";
+    }
+}
