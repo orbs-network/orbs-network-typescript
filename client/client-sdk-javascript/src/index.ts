35b097e7814d429169baf95613a1e2b30fa7f8e5
idandush <idanofrat@gmail.com>
2018-04-02 17:36:00 +0300

    minor export fix

--- client/client-sdk-javascript/src/index.ts
+++ client/client-sdk-javascript/src/index.ts
@@ -1,3 +1,3 @@
 export { OrbsClientSession } from "./orbs-client";
 export { initPublicApiClient } from "./public-api-client";
-import OrbsContractAdapter from "./orbs-contract-adapter";
+export { OrbsContractAdapter } from "./orbs-contract-adapter";
