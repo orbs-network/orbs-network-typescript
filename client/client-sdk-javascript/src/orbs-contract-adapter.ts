326e3785770f6e169a5099c43c6b002ba7ff76f0
idandush <idanofrat@gmail.com>
2018-04-02 16:02:00 +0300

    minor refactoring

---
+++ client/client-sdk-javascript/src/orbs-contract-adapter.ts
@@ -0,0 +1,30 @@
+import { OrbsClientSession } from ".";
+import { SendTransactionOutput } from "orbs-interfaces";
+
+export type OrbsContractMethodArgs = [string | number] | any[];
+
+export default class OrbsContractAdapter {
+  orbsSession: OrbsClientSession;
+  contractAddress: string;
+
+  constructor(orbsSession: OrbsClientSession, contractAddress: string) {
+    this.orbsSession = orbsSession;
+    this.contractAddress = contractAddress;
+  }
+
+  public async sendTransaction(methodName: string, args: OrbsContractMethodArgs): Promise<SendTransactionOutput> {
+    const payload = JSON.stringify({
+      method: methodName,
+      args: args
+    });
+    return await this.orbsSession.sendTransaction(this.contractAddress, payload);
+  }
+
+  public async call(methodName: string, args: OrbsContractMethodArgs) {
+    const payload = JSON.stringify({
+      method: methodName,
+      args: args
+    });
+    return this.orbsSession.call(this.contractAddress, payload);
+  }
+}
