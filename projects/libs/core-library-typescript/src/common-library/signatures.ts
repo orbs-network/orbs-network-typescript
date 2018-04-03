0e65d692eb6678c6f792d1abe39aa2f51bbeba2b
Kirill Maksimov <kirill@orbs.com>
2018-04-03 11:31:55 +0300

    Store keys in memory (we might want to change it later)

--- projects/libs/core-library-typescript/src/common-library/signatures.ts
+++ projects/libs/core-library-typescript/src/common-library/signatures.ts
@@ -20,13 +20,29 @@ export class Signatures {
   SIGNATURE_FORMAT: crypto.HexBase64Latin1Encoding = "base64";
   SIGNATURE_TYPE = "sha256";
 
+  private publicMessageKeys = new Map<string, string>();
+  private privateMessageKey: string;
+
   public constructor(signaturesConfig: SignaturesConfig) {
     this.config = signaturesConfig;
+
+    this.readPrivateMessageKey();
+    this.readPublicMessageKeys();
   }
 
-  private getKey(path: string) {
-    const rawKey = fs.readFileSync(path).toString();
-    return rawKey;
+  private readPrivateMessageKey() {
+    if (this.config.message && this.config.message.privateKeyPath) {
+      this.privateMessageKey = fs.readFileSync(this.config.message.privateKeyPath).toString();
+    }
+  }
+
+  private readPublicMessageKeys() {
+    if (this.config.message && this.config.message.publicKeysPath) {
+      fs.readdirSync(this.config.message.publicKeysPath).forEach((keyName) => {
+        const contents = fs.readFileSync(`${this.config.message.publicKeysPath}/${keyName}`).toString();
+        this.publicMessageKeys.set(keyName, contents);
+      });
+    }
   }
 
   private signObject(object: any, key: string): string {
@@ -45,21 +61,12 @@ export class Signatures {
     return verify.verify(publicKey, signature, this.SIGNATURE_FORMAT);
   }
 
-  private getPublicKey(path: string, keyName: string): string {
-    if (!keyName.match(/[a-zA-Z-_0-9]/)) {
-      throw new Error(`Bad public key name: ${keyName}`);
-    }
-
-    return this.getKey(`${path}/${keyName}`);
-  }
-
   public signMessage(object: any): string {
-    const privateKey = this.getKey(this.config.message.privateKeyPath);
-    return this.signObject(object, privateKey);
+    return this.signObject(object, this.privateMessageKey);
   }
 
   public verifyMessage(object: any, signature: string, publicKeyName: string): boolean {
-    const publicKey = this.getPublicKey(this.config.message.publicKeysPath, publicKeyName);
+    const publicKey = this.publicMessageKeys.get(publicKeyName);
     return this.verifyObject(object, signature, publicKey);
   }
 }
