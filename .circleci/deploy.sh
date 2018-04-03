d3fa2bd56d39857dcc8e4cb2a9bd562fd78d80f3
Kirill Maksimov <kirill@orbs.com>
2018-04-03 15:55:24 +0300

    Add dummy keys to deployment process

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,4 +2,7 @@
 
 cd deploy
 npm install
+
+aws s3 sync s3://orbs-network-config-staging/dummy-keys/public-keys/ bootstrap/public-keys/
+aws s3 sync s3://orbs-network-config-staging/dummy-keys/private-keys/ temp-keys/private-keys/
 ./deploy-staging.sh
