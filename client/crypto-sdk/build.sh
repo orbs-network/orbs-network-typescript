f4c2c22ffd82f7d1313cb2145a024e76c452e2be
Leonid Beder <leonid@orbs.com>
2018-04-08 17:39:35 +0900

    Refactor build

--- client/crypto-sdk/build.sh
+++ client/crypto-sdk/build.sh
@@ -1,5 +1,30 @@
 #!/bin/bash -e
 
+function build_ios() {
+    echo "Building for ${IOS_PLATFORM}..."
+
+    (cd ../ && CMAKE_ONLY=1 ./clean.sh)
+    cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DPLATFORM=IOS -DIOS_PLATFORM=${IOS_PLATFORM} -DCMAKE_TOOLCHAIN_FILE="toolchains/ios.toolchain.cmake"
+    make
+}
+
+function build_android() {
+    echo "Building for ${ANDROID_ABI}..."
+
+    case "$(uname -s)" in
+        Darwin)
+            ANDROID_NDK_HOME=~/Library/Android/sdk/ndk-bundle
+
+            ;;
+        *)
+            ;;
+    esac
+
+    (cd ../ && CMAKE_ONLY=1 ./clean.sh)
+    cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DPLATFORM=ANDROID -DANDROID_ABI=${ANDROID_ABI} -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake"
+    make
+}
+
 ./build-deps.sh
 
 pushd build
@@ -14,36 +39,22 @@ echo "Building ${BUILD_TYPE} version for ${PLATFORM:-$(uname -s)}..."
 
 case ${PLATFORM} in
     IOS)
-        (cd ../ && CMAKE_ONLY=1 ./clean.sh)
-        cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DPLATFORM=IOS -DCMAKE_TOOLCHAIN_FILE="toolchains/ios.toolchain.cmake" -DIOS_PLATFORM="iPhoneSimulator"
-        make
+        IOS_PLATFORM="iPhoneSimulator"
+        build_ios
 
         ;;
     ANDROID)
-        case "$(uname -s)" in
-            Darwin)
-                ANDROID_NDK_HOME=~/Library/Android/sdk/ndk-bundle
+        ANDROID_ABI="armeabi-v7a"
+        build_android
 
-                ;;
-            *)
-                ;;
-        esac
+        ANDROID_ABI="arm64-v8a"
+        build_android
 
-        (cd ../ && CMAKE_ONLY=1 ./clean.sh)
-        cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DPLATFORM=ANDROID -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake"
-        make
+        ANDROID_ABI="x86"
+        build_android
 
-        (cd ../ && CMAKE_ONLY=1 ./clean.sh)
-        cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DPLATFORM=ANDROID -DANDROID_ABI=arm64-v8a -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake"
-        make
-
-        (cd ../ && CMAKE_ONLY=1 ./clean.sh)
-        cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DPLATFORM=ANDROID -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake"
-        make
-
-        (cd ../ && CMAKE_ONLY=1 ./clean.sh)
-        cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DPLATFORM=ANDROID -DANDROID_ABI=x86_64 -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake"
-        make
+        ANDROID_ABI="x86_64"
+        build_android
 
         ;;
     *)
