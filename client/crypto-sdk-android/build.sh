5b660e43ea725a6b643e7c939e431cfae3efd8a4
Leonid Beder <leonid@orbs.com>
2018-04-11 19:22:35 +0300

    Support local build for testing.

--- client/crypto-sdk-android/build.sh
+++ client/crypto-sdk-android/build.sh
@@ -9,28 +9,41 @@ if [ -z "${PLATFORM}" ]; then
 fi
 
 # Copy external crypto-sdk dependencies to jniLibs.
-JNILIBS_DIR=crypto-sdk/src/main/jniLibs/
-CRYPTO_SDK_BUILD_DIR=../crypto-sdk/build/android
-mkdir -p ${JNILIBS_DIR}armeabi-v7a/ ${JNILIBS_DIR}arm64-v8a/ ${JNILIBS_DIR}x86/ ${JNILIBS_DIR}x86_64/
-cp -f ${CRYPTO_SDK_BUILD_DIR}/armv7-a/lib/libcryptosdk.so ${JNILIBS_DIR}armeabi-v7a/
-cp -f ${CRYPTO_SDK_BUILD_DIR}/armv8-a/lib/libcryptosdk.so ${JNILIBS_DIR}arm64-v8a/
-cp -f ${CRYPTO_SDK_BUILD_DIR}/i686/lib/libcryptosdk.so ${JNILIBS_DIR}x86/
-cp -f ${CRYPTO_SDK_BUILD_DIR}/westmere/lib/libcryptosdk.so ${JNILIBS_DIR}x86_64/
+JNILIBS_DIR=crypto-sdk/src/main/jniLibs
+CRYPTO_SDK_BUILD_DIR=../crypto-sdk/build
+CRYPTO_SDK_ANDROID_BUILD_DIR=${CRYPTO_SDK_BUILD_DIR}/android
+CRYPTO_SDK_JAVA_BUILD_DIR=crypto-sdk/build/outputs/jar
+mkdir -p ${JNILIBS_DIR}/armeabi-v7a/ ${JNILIBS_DIR}/arm64-v8a/ ${JNILIBS_DIR}/x86/ ${JNILIBS_DIR}/x86_64/
+cp -f ${CRYPTO_SDK_ANDROID_BUILD_DIR}/armv7-a/lib/libcryptosdk.so ${JNILIBS_DIR}/armeabi-v7a/
+cp -f ${CRYPTO_SDK_ANDROID_BUILD_DIR}/armv8-a/lib/libcryptosdk.so ${JNILIBS_DIR}/arm64-v8a/
+cp -f ${CRYPTO_SDK_ANDROID_BUILD_DIR}/i686/lib/libcryptosdk.so ${JNILIBS_DIR}/x86/
+cp -f ${CRYPTO_SDK_ANDROID_BUILD_DIR}/westmere/lib/libcryptosdk.so ${JNILIBS_DIR}/x86_64/
 
 case "$(uname -s)" in
     Darwin)
         LOCAL_PLATFORM="mac"
+        LOCAL_LIBRARY="${LOCAL_PLATFORM}/lib/libcryptosdk.dylib"
 
         ;;
     Linux)
         LOCAL_PLATFORM="linux"
+        LOCAL_LIBRARY="${LOCAL_PLATFORM}/lib/libcryptosdk.so"
 
         ;;
     *)
-        echo "Unsupport system ${SYSNAME}!"
+        echo "Unsupported system ${SYSNAME}!"
         exit 1
 
         ;;
 esac
 
-gradle build
+mkdir -p ${CRYPTO_SDK_JAVA_BUILD_DIR}
+cp -f ${CRYPTO_SDK_BUILD_DIR}/${LOCAL_LIBRARY} ${CRYPTO_SDK_JAVA_BUILD_DIR}
+
+if [ -n "${DEBUG}" ] ; then
+    BUILD_TYPE=Debug
+else
+    BUILD_TYPE=Release
+fi
+
+gradle assemble${BUILD_TYPE}
