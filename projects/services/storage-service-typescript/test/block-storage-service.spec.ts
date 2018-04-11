754e49c06c5c54dd90cdb8ae24af7d621517d894
Jonathan Levison <jonathanl@orbs.com>
2018-04-11 17:03:15 +0300

    refactored storage service tests

--- projects/services/storage-service-typescript/test/block-storage-service.spec.ts
+++ projects/services/storage-service-typescript/test/block-storage-service.spec.ts
@@ -1,46 +1,89 @@
-import { types, ErrorHandler } from "orbs-core-library";
 import * as chai from "chai";
-import * as chaiAsPromised from "chai-as-promised";
-import { delay } from "bluebird";
-import { initNodesWithBlocks, NodeLoader } from "./node-loader";
+import * as fs from "fs";
+import * as path from "path";
+import * as getPort from "get-port";
+import { stubInterface } from "ts-sinon";
+
+import { types, BlockUtils, ErrorHandler, GRPCServerBuilder, grpc } from "orbs-core-library";
+import { BlockStorageClient, StateStorageClient, GossipServer } from "orbs-interfaces";
+import storageServer from "../src/server";
+import GossipService from "../../gossip-service-typescript/src/service";
+import TransactionPoolService from "../../consensus-service-typescript/src/transaction-pool-service";
 
 const { expect } = chai;
 
 ErrorHandler.setup();
 
-chai.use(chaiAsPromised);
+describe("new storage server test", function () {
+  this.timeout(10000);
+  let server: GRPCServerBuilder;
+  let blockClient: BlockStorageClient;
+  let stateClient: StateStorageClient;
 
-describe("Block storage service", async function () {
-  this.timeout(200000);
+  before(async () => {
+    const endpoint = `127.0.0.1:${await getPort()}`;
 
-  describe("sync process", () => {
-    let nodes: NodeLoader[] = [];
+    const topology =  {
+      peers: [
+        {
+          service: "storage",
+          endpoint: endpoint,
+        },
+        {
+          service: "gossip",
+          endpoint: endpoint,
+        },
+        {
+          service: "consensus",
+          endpoint: endpoint,
+        },
+      ],
+    };
 
-    it("works with two nodes", async () => {
-      nodes = await initNodesWithBlocks([10, 20]);
+    const NODE_NAME = "tester";
+    const BLOCK_STORAGE_POLL_INTERVAL = 5000;
+    const BLOCK_STORAGE_DB_PATH = "./test/db/";
+    const storageEnv = { NODE_NAME, BLOCK_STORAGE_POLL_INTERVAL, BLOCK_STORAGE_DB_PATH };
+    const gossipServerStub = stubInterface<GossipService>();
+    const transactionPoolStub = stubInterface<TransactionPoolService>();
 
-      const values = delay(2000).then(() => Promise.all(nodes.map(node => node.getLastBlockHeight())));
+    // handle the filesystem for this test, will empty the db folder before starting the services
+    // TODO: move outside, talk to kirill
+    if (!fs.existsSync(BLOCK_STORAGE_DB_PATH)) {
+      fs.mkdirSync(BLOCK_STORAGE_DB_PATH);
+    }
+    const files = fs.readdirSync(BLOCK_STORAGE_DB_PATH).map(f => path.join(BLOCK_STORAGE_DB_PATH, f));
+    files.forEach(f => fs.unlinkSync(f));
 
-      return expect(values).to.eventually.be.eql([20, 20]);
+    server = storageServer(topology, storageEnv)
+      .withService("Gossip", gossipServerStub)
+      .withService("TransactionPool", transactionPoolStub)
+      .onEndpoint(endpoint);
+    server.start();
 
-    });
+    blockClient = grpc.blockStorageClient({ endpoint });
+    stateClient = grpc.stateStorageClient({ endpoint });
+  });
 
-    it("works with multiple nodes", async () => {
-      nodes = await initNodesWithBlocks([10, 20, 50, 100]);
+  it("should fetch genesis block for an empty database", async () => {
+    const lastBlock = await blockClient.getLastBlock({});
+    expect(lastBlock.block.header.height).to.equal(0);
+  });
 
-      const values = delay(2000).then(() => Promise.all(nodes.map(node => node.getLastBlockHeight())));
+  it("checks state storage", async () => {
+    const lastBlock = await blockClient.getLastBlock({});
+    const nextBlock = BlockUtils.buildNextBlock({
+      transactions: [],
+      transactionReceipts: [],
+      stateDiff: []
+    }, lastBlock.block);
+    blockClient.addBlock({ block: nextBlock });
 
-      return expect(values).to.eventually.be.eql([100, 100, 100, 100]);
-    });
+    const state = await stateClient.readKeys({ contractAddress: { address: "does-not-exist" }, keys: [] });
+    return expect(state).to.have.deep.property("values", {});
+  });
 
-    xit("finishes if we keep adding more and more blocks to other nodes");
-
-    xit("finishes even if node we sync from is dead");
-
-    xit("successfully adds new blocks during sync");
-
-    afterEach(async () => {
-      await Promise.all(nodes.map(node => node.cleanup()));
-    });
+  after(() => {
+    return server.stop();
   });
 });
