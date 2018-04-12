3f56d7e1e101b7a2ab3e8f2bee417be0bf7c41e7
Kirill Maksimov <kirill@orbs.com>
2018-04-12 17:52:17 +0300

    Finish contract test and mock server

--- client/client-sdk-javascript/test/client-sdk.spec.ts
+++ client/client-sdk-javascript/test/client-sdk.spec.ts
@@ -4,10 +4,11 @@ import { Address } from "../src/address";
 import { ED25519Key } from "../src/ed25519key";
 import { OrbsClient } from "../src";
 import PublicApiConnection from "../src/public-api-connection";
-import { stubInterface } from "ts-sinon";
 import * as sinonChai from "sinon-chai";
 import * as nock from "nock";
-import { ContractAddress, Transaction, SendTransactionInput, CallContractInput } from "orbs-interfaces";
+import { ContractAddress, Transaction, SendTransactionInput, CallContractInput, UniversalAddress } from "orbs-interfaces";
+import mockHttpServer from "./mock-server";
+import { Server } from "http";
 
 chai.use(sinonChai);
 
@@ -17,37 +18,53 @@ const senderAddress = new Address(publicKey, VIRTUAL_CHAIN_ID, Address.TEST_NETW
 const API_ENDPOINT = "http://localhost:8888";
 const TIMEOUT = 20;
 
+const universalAddress: UniversalAddress = {
+  id: new Buffer(senderAddress.toString()),
+  networkId: Number(Address.TEST_NETWORK_ID),
+  checksum: 0,
+  scheme: 0
+};
+
+const expectedTransaction: Transaction = {
+  header: {
+    version: 0,
+    sender: universalAddress,
+    timestamp: Date.now().toString()
+  },
+  body: {
+    contractAddress: {
+      address: "contractAddress"
+    },
+    payload: "payload"
+  }
+};
+
+const expectedContract: CallContractInput = {
+  sender: universalAddress,
+  contractAddress: {
+    address: "contractAddress"
+  },
+  payload: "call-payload"
+};
+
 describe("A client calls the connector interface with the correct inputs when", async function () {
   let orbsClient: OrbsClient;
-  const connection = stubInterface<PublicApiConnection>();
+  let httpServer: Server;
 
   beforeEach(async () => {
     orbsClient = new OrbsClient(API_ENDPOINT, senderAddress.toString(), TIMEOUT);
+    httpServer = mockHttpServer(expectedTransaction, expectedContract).listen(8888);
   });
 
   it("sendTransaction() is called", async () => {
-    nock(API_ENDPOINT)
-      .post("/public/sendTransaction", (res: any) => {
-        expect(res.transaction.body).to.be.eql({
-          contractAddress: {
-            address: "contractAddress"
-          },
-          payload: "payload"
-        });
-        return true;
-      }).reply(200, { result: "ok" });
-
     expect(await orbsClient.sendTransaction("contractAddress", "payload")).to.be.eql("ok");
   });
 
   it("callContract() is called", async () => {
-    nock(API_ENDPOINT)
-      .post("/public/callContract", (res: any) => {
-        expect(res.contractAddress).to.be.eql({ address: "contractAddress" });
-        expect(res.payload).to.be.eql("call-payload");
-        return true;
-      }).reply(200, { result: 12 });
+    expect(await orbsClient.call("contractAddress", "call-payload")).to.be.eql("some-answer");
+  });
 
-    expect(await orbsClient.call("contractAddress", "call-payload")).to.be.eql(12);
+  afterEach(async () => {
+    httpServer.close();
   });
 });
--- client/client-sdk-javascript/test/mock-server.ts
+++ client/client-sdk-javascript/test/mock-server.ts
@@ -10,14 +10,16 @@ export default function createMockServer(expectedTransaction: Transaction, expec
   app.use(express.json());
 
   app.use("/public/sendTransaction", (req, res) => {
+    // TODO: check header
     expect(req.body.transaction.body).to.be.eql(expectedTransaction.body);
 
     res.json({ result: "ok" });
   });
 
   app.use("/public/callContract", (req, res) => {
-    expect(req.body.contractAddress).to.be.eql({ address: "contractAddress" });
-    expect(req.body.payload).to.be.eql("some-payload");
+    // TODO: check sender
+    expect(req.body.contractAddress).to.be.eql(expectedContract.contractAddress);
+    expect(req.body.payload).to.be.eql(expectedContract.payload);
 
     res.json({ result: "some-answer" });
   });
