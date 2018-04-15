5abc87f55ec41646aea3d752f53d699bf68b03dd
Jonathan Levison <jonathanl@orbs.com>
2018-04-15 13:58:04 +0300

    added a check in state storage to avoid null reference when the block storage did not finish initialization and the state storage is already polling

--- projects/libs/core-library-typescript/src/block-storage/block-storage.ts
+++ projects/libs/core-library-typescript/src/block-storage/block-storage.ts
@@ -82,8 +82,11 @@ export class BlockStorage {
   public async getBlocks(fromLastBlockHeight: number): Promise<types.Block[]> {
     const blocks: types.Block[] = [];
 
-    for (let i = fromLastBlockHeight; i < this.lastBlock.header.height; ++i) {
-      blocks.push(await this.getBlock(i + 1));
+    // lastBlock is undefined when the service did not initialize the logic/load() did not run yet (race condition most likely)
+    if (this.lastBlock != undefined) {
+      for (let i = fromLastBlockHeight; i < this.lastBlock.header.height; ++i) {
+        blocks.push(await this.getBlock(i + 1));
+      }
     }
 
     return blocks;
