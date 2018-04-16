3614ab45e6a22659fca85a536fde11761ec0714b
Kirill Maksimov <kirill@orbs.com>
2018-04-16 14:18:39 +0300

    Extract server build

--- docker-build.sh
+++ docker-build.sh
@@ -5,4 +5,4 @@ export DOCKER_TAG=${DOCKER_TAG-$(./docker-tag.sh)}
 export DOCKER_BUILD_OPTIONS=${DOCKER_BUILD_OPTIONS-""}
 # --squash to produce a single layer image
 
-docker build $DOCKER_BUILD_OPTIONS -t ${DOCKER_IMAGE}:${DOCKER_TAG} --build-arg CI=${CI} .
+docker build $DOCKER_BUILD_OPTIONS -f Dockerfile.server -t ${DOCKER_IMAGE}:${DOCKER_TAG} --build-arg CI=${CI} .
