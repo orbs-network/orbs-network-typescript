811b3480e02706bdb450b8f646738814d85253b0
Jonathan Levison <jonathanl@orbs.com>
2018-04-17 11:15:49 +0300

    gossip test skeleton fires service, ready to implement tests

--- projects/services/gossip-service-typescript/test/gossip-service.spec.ts
+++ projects/services/gossip-service-typescript/test/gossip-service.spec.ts
@@ -3,43 +3,50 @@ import * as chai from "chai";
 import * as getPort from "get-port";
 import { stubInterface } from "ts-sinon";
 
-import { types, BlockUtils, ErrorHandler, GRPCServerBuilder, grpc, logger, createContractAddress } from "orbs-core-library";
+import { types, ErrorHandler, GRPCServerBuilder, grpc, logger } from "orbs-core-library";
 import { BlockStorageClient, StateStorageClient } from "orbs-interfaces";
 import GossipService from "../src/service";
+import gossipServer from "../src/server";
+import ConsensusService from "../../consensus-service-typescript/src/consensus-service";
 
 const { expect } = chai;
 
 ErrorHandler.setup();
 
+logger.configure({ level: "debug" });
+
 describe("gossip server test", function () {
-  // let server: GRPCServerBuilder;
+  let server: GRPCServerBuilder;
 
   beforeEach(async () => {
-    const endpoint = `127.0.0.1:${await getPort()}`;
+    const gossipPort = await getPort();
+    const endpoint = `127.0.0.1:${gossipPort}`;
+    const anotherEndpoint = `127.0.0.1:${await getPort()}`;
 
     const topology =  {
       peers: [
         {
-          service: "storage",
-          endpoint: endpoint,
-        },
-        {
-          service: "gossip",
-          endpoint: endpoint,
-        },
-        {
           service: "consensus",
           endpoint: endpoint,
         },
+        {
+          service: "storage",
+          endpoint: endpoint,
+        },
       ],
+      gossipPort: gossipPort,
+      gossipPeers: [ anotherEndpoint ]
     };
 
     const NODE_NAME = "tester";
-    const BLOCK_STORAGE_POLL_INTERVAL = 5000;
-    const STATE_STORAGE_POLL_INTERVAL = 200;
-    const storageEnv = { NODE_NAME, BLOCK_STORAGE_POLL_INTERVAL,  STATE_STORAGE_POLL_INTERVAL };
-    const gossipServerStub = stubInterface<GossipService>();
+    const SIGN_MESSAGES = false;
+    const gossipEnv = { NODE_NAME, SIGN_MESSAGES };
+    const consensusServiceStub = stubInterface<ConsensusService>();
 
+    server = gossipServer(topology, gossipEnv)
+      .withService("Consensus", consensusServiceStub);
+
+    return server.start();
   });
 
   it("dummy test", async () => {
@@ -47,5 +54,6 @@ describe("gossip server test", function () {
   });
 
   afterEach(() => {
+    return server.stop();
   });
 });
