cab28abb2bca3a4f4ed28bab704af28889f6022c
Jonathan Levison <jonathanl@orbs.com>
2018-04-17 11:08:11 +0300

    cleanup after refactor to grpcserverbuilder (WIP)

--- projects/services/gossip-service-typescript/src/index.ts
+++ projects/services/gossip-service-typescript/src/index.ts
@@ -1,11 +1,8 @@
 import * as path from "path";
 
-import { logger, ErrorHandler, grpc, Service, ServiceRunner, topology, topologyPeers, KeyManager } from "orbs-core-library";
+import { logger, ErrorHandler, Service, topology } from "orbs-core-library";
 
-import GossipService from "./service";
 import gossipServer from "./server";
-
-const { NODE_NAME, SIGN_MESSAGES } = process.env;
 
 ErrorHandler.setup();
 
