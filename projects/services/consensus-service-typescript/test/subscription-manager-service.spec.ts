b7d8bddf9f953b3180606485c102b920f3001feb
Jonathan Levison <jonathanl@orbs.com>
2018-04-24 13:33:57 +0300

    stable tests for the subscription manager

--- projects/services/consensus-service-typescript/test/subscription-manager-service.spec.ts
+++ projects/services/consensus-service-typescript/test/subscription-manager-service.spec.ts
@@ -3,9 +3,11 @@ import * as chai from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import * as getPort from "get-port";
 import { stubInterface } from "ts-sinon";
+import * as sinon from "sinon";
+import BigNumber from "bignumber.js";
 
-import { types, ErrorHandler, GRPCServerBuilder, grpc, logger, Gossip } from "orbs-core-library";
-
+import { types, ErrorHandler, GRPCServerBuilder, grpc, logger, SubscriptionManager, grpcServer } from "orbs-core-library";
+import SubscriptionManagerService from "../src/subscription-manager-service";
 
 const { expect } = chai;
 
@@ -14,7 +16,57 @@ ErrorHandler.setup();
 logger.configure({ level: "debug" });
 
 describe("subscription manager service tests", function() {
-    it("should get subscription status", () => {
-        expect(1).to.be.equal(2);
+    let server: GRPCServerBuilder;
+    let client: types.SubscriptionManagerClient;
+    let subscriptionManagerStub: SubscriptionManager;
+
+
+    beforeEach(async () => {
+        const endpoint = `127.0.0.1:${await getPort()}`;
+        subscriptionManagerStub = stubInterface<SubscriptionManager>();
+
+        server = grpcServer.builder()
+            .withService("SubscriptionManager", new SubscriptionManagerService(subscriptionManagerStub, { nodeName: "tester"}))
+            .onEndpoint(endpoint);
+
+        client = grpc.subscriptionManagerClient({ endpoint });
+
+        return server.start();
+
+    });
+
+    it("should get subscription status", async () => {
+        (<sinon.SinonStub>subscriptionManagerStub.getSubscriptionStatus).returns({ id: 1, tokens: new BigNumber(2) });
+
+        const subData = await client.getSubscriptionStatus({ subscriptionKey: "abc" });
+
+        expect(subData).to.have.nested.property("active", true);
+        expect(subData).to.have.nested.property("expiryTimestamp");
+    });
+
+    it("should expire in 24 minutes", async () => {
+        (<sinon.SinonStub>subscriptionManagerStub.getSubscriptionStatus).returns({ id: 1, tokens: new BigNumber(2) });
+
+        const subData = await client.getSubscriptionStatus({ subscriptionKey: "abc" });
+        logger.info(JSON.stringify(subData));
+        expect(subData).to.have.nested.property("active", true);
+        expect(subData).to.have.nested.property("expiryTimestamp");
+        const timein23mins = Date.now() + 23 * 60 * 1000;
+        expect(subData.expiryTimestamp).to.be.greaterThan(timein23mins);
+        const timein25mins = Date.now() + 25 * 60 * 1000;
+        expect(subData.expiryTimestamp).to.be.lessThan(timein25mins);
+    });
+
+    it("should be inactive if no tokens", async () => {
+        (<sinon.SinonStub>subscriptionManagerStub.getSubscriptionStatus).returns({ id: 1, tokens: new BigNumber(0) });
+
+        const subData = await client.getSubscriptionStatus({ subscriptionKey: "abc" });
+
+        expect(subData).to.have.nested.property("active", false);
+        expect(subData).to.have.nested.property("expiryTimestamp");
+    });
+
+    afterEach(async () => {
+        return server.stop();
     });
 });
