c2de8a7ae50cab793556be9e4674523ec4fff327
Kirill Maksimov <kirill@orbs.com>
2018-04-24 21:09:26 +0900

    Add vm provisioning through Vagrant (WIP)

---
+++ deploy/local/bootstrap-local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apt-get install -y python-pip && pip install docker-compose && (curl -fsSL get.docker.com | bash)
---
+++ docker-export.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export DOCKER_IMAGE=${DOCKER_IMAGE-orbs}
+export DOCKER_TAG=${DOCKER_TAG-$(./docker-tag.sh)}
+
+mkdir -p docker/images
+
+docker image save $DOCKER_IMAGE:$DOCKER_TAG -o docker/images/orbs-network.tar
+echo export DOCKER_IMAGE=$DOCKER_IMAGE DOCKER_TAG=$DOCKER_TAG > docker/images/env
