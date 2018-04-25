511b55f13f720276b696c66ca8c9effea126f7ac
Kirill Maksimov <kirill@orbs.com>
2018-04-25 14:24:13 +0900

    Add docs, verify working installation

--- docker-export.sh
+++ docker-export.sh
@@ -6,4 +6,4 @@ export DOCKER_TAG=${DOCKER_TAG-$(./docker-tag.sh)}
 mkdir -p docker/images
 
 docker image save $DOCKER_IMAGE:$DOCKER_TAG -o docker/images/orbs-network.tar
-echo export DOCKER_IMAGE=$DOCKER_IMAGE DOCKER_TAG=$DOCKER_TAG > docker/images/env
+echo DOCKER_IMAGE=$DOCKER_IMAGE DOCKER_TAG=$DOCKER_TAG > docker/images/.env
