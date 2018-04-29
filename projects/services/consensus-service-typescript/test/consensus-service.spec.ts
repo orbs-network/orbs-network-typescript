eadd254cff78cf180a8da180c452453189c63b0a
Jonathan Levison <jonathanl@orbs.com>
2018-04-29 14:42:08 +0300

    consensus test infrastructure (WIP)

---
+++ projects/services/consensus-service-typescript/test/consensus-service.spec.ts
@@ -0,0 +1,69 @@
+import * as mocha from "mocha";
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+import * as getPort from "get-port";
+import { stubInterface } from "ts-sinon";
+import * as sinon from "sinon";
+
+import { types, ErrorHandler, GRPCServerBuilder, grpc, logger } from "orbs-core-library";
+import { GossipClient } from "orbs-interfaces";
+
+import consensusServer from "../src/consensus-server";
+import GossipService from "../../gossip-service-typescript/src/service";
+
+
+const { expect } = chai;
+
+ErrorHandler.setup();
+
+logger.configure({ level: "debug" });
+
+chai.use(chaiAsPromised);
+
+describe("consensus service tests", function() {
+    let server: GRPCServerBuilder;
+
+    beforeEach(async () => {
+        const endpoint = `127.0.0.1:${await getPort()}`;
+
+        const fakeTopology = {
+            peers: [
+                {
+                  service: "gossip",
+                  endpoint: endpoint,
+                },
+                {
+                  service: "storage",
+                  endpoint: endpoint,
+                },
+                {
+                  service: "sidechain-connector",
+                  endpoint: endpoint,
+                },
+                {
+                  service: "consensus",
+                  endpoint: endpoint,
+                },
+                {
+                  service: "virtual-machine",
+                  endpoint: endpoint,
+                },
+              ]
+        };
+
+        const gossipServerStub = stubInterface<GossipService>();
+
+        server = consensusServer(fakeTopology)
+            .withService("Gossip", gossipServerStub);
+
+        return server.start();
+    });
+
+    it("should be able to receive a gossip message", async () => {
+        expect(1).to.be.equal(2);
+    });
+
+    afterEach(async () => {
+        return server.stop();
+    });
+});
