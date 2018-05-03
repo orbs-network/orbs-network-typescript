e3a58dfb258d497cd73e0847432ef85db58beda8
Leonid Beder <leonid@orbs.com>
2018-05-03 19:11:35 +0300

    Refactor CRC32 tests

--- client/crypto-sdk/test/crc32-test.cpp
+++ client/crypto-sdk/test/crc32-test.cpp
@@ -2,12 +2,11 @@
 #include <gtest/gtest.h>
 
 #include "../lib/crc32.h"
+#include "../lib/utils.h"
 
 using namespace std;
 using namespace testing;
 using namespace Orbs;
-
-static const uint CRC32_DIGEST_LENGTH = 4;
 
 TEST(Hash, computes_binary_CRC32_of_binary) {
     vector<uint8_t> res;
@@ -16,38 +15,38 @@ TEST(Hash, computes_binary_CRC32_of_binary) {
     uint8_t rawData[] = "";
     data = vector<uint8_t>(rawData, rawData + sizeof(rawData) - 1);
     CRC32::Hash(data, res);
-    EXPECT_THAT(res, ElementsAreArray("\x00\x00\x00\x00", CRC32_DIGEST_LENGTH));
+    EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("00000000")));
 
     uint8_t rawData2[] = "Hello World!";
     data = vector<uint8_t>(rawData2, rawData2 + sizeof(rawData2) - 1);
     CRC32::Hash(data, res);
-    EXPECT_THAT(res, ElementsAreArray("\x1c\x29\x1c\xa3", CRC32_DIGEST_LENGTH));
+    EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("1c291ca3")));
 
     uint8_t rawData3[] = "If I sign myself Jean-Paul Sartre it is not the same thing as if I sign myself Jean-Paul Sartre, Nobel Prize winner.";
     data = vector<uint8_t>(rawData3, rawData3 + sizeof(rawData3) - 1);
     CRC32::Hash(data, res);
-    EXPECT_THAT(res, ElementsAreArray("\x75\xe3\x3a\xef", CRC32_DIGEST_LENGTH));
+    EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("75e33aef")));
 
     uint8_t rawData4[] = "אורבס";
     data = vector<uint8_t>(rawData4, rawData4 + sizeof(rawData4) - 1);
     CRC32::Hash(data, res);
-    EXPECT_THAT(res, ElementsAreArray("\x42\x6c\xf3\x67", CRC32_DIGEST_LENGTH));
+    EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("426cf367")));
 }
 
 TEST(Hash, computes_binary_CRC32_of_string) {
     vector<uint8_t> res;
 
     CRC32::Hash("", res);
-    EXPECT_THAT(res, ElementsAreArray("\x00\x00\x00\x00", CRC32_DIGEST_LENGTH));
+    EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("00000000")));
 
     CRC32::Hash("Hello World!", res);
-    EXPECT_THAT(res, ElementsAreArray("\x1c\x29\x1c\xa3", CRC32_DIGEST_LENGTH));
+    EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("1c291ca3")));
 
     CRC32::Hash("If I sign myself Jean-Paul Sartre it is not the same thing as if I sign myself Jean-Paul Sartre, Nobel Prize winner.", res);
-    EXPECT_THAT(res, ElementsAreArray("\x75\xe3\x3a\xef", CRC32_DIGEST_LENGTH));
+    EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("75e33aef")));
 
     CRC32::Hash("אורבס", res);
-    EXPECT_THAT(res, ElementsAreArray("\x42\x6c\xf3\x67", CRC32_DIGEST_LENGTH));
+    EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("426cf367")));
 }
 
 TEST(Hash, computes_string_CRC32_of_binary) {
@@ -90,4 +89,3 @@ TEST(Hash, computes_string_CRC32_of_string) {
     CRC32::Hash("אורבס", res);
     EXPECT_STREQ(res.c_str(), "426cf367");
 }
-
