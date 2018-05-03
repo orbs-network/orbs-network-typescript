b8e97df68799f84885e52919954952e52d5b87d8
Leonid Beder <leonid@orbs.com>
2018-05-03 19:28:57 +0300

    Refactor remaining tests

--- client/crypto-sdk/test/crc32-test.cpp
+++ client/crypto-sdk/test/crc32-test.cpp
@@ -10,26 +10,24 @@ using namespace Orbs;
 
 TEST(Hash, computes_binary_CRC32_of_binary) {
     vector<uint8_t> res;
-    vector<uint8_t> data;
 
-    uint8_t rawData[] = "";
-    data = vector<uint8_t>(rawData, rawData + sizeof(rawData) - 1);
-    CRC32::Hash(data, res);
+    vector<uint8_t> data1;
+    CRC32::Hash(data1, res);
     EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("00000000")));
 
     uint8_t rawData2[] = "Hello World!";
-    data = vector<uint8_t>(rawData2, rawData2 + sizeof(rawData2) - 1);
-    CRC32::Hash(data, res);
+    vector<uint8_t> data2(rawData2, rawData2 + sizeof(rawData2) - 1);
+    CRC32::Hash(data2, res);
     EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("1c291ca3")));
 
     uint8_t rawData3[] = "If I sign myself Jean-Paul Sartre it is not the same thing as if I sign myself Jean-Paul Sartre, Nobel Prize winner.";
-    data = vector<uint8_t>(rawData3, rawData3 + sizeof(rawData3) - 1);
-    CRC32::Hash(data, res);
+    vector<uint8_t> data3(rawData3, rawData3 + sizeof(rawData3) - 1);
+    CRC32::Hash(data3, res);
     EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("75e33aef")));
 
     uint8_t rawData4[] = "אורבס";
-    data = vector<uint8_t>(rawData4, rawData4 + sizeof(rawData4) - 1);
-    CRC32::Hash(data, res);
+    vector<uint8_t> data4(rawData4, rawData4 + sizeof(rawData4) - 1);
+    CRC32::Hash(data4, res);
     EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("426cf367")));
 }
 
@@ -51,26 +49,24 @@ TEST(Hash, computes_binary_CRC32_of_string) {
 
 TEST(Hash, computes_string_CRC32_of_binary) {
     string res;
-    vector<uint8_t> data;
 
-    uint8_t rawData[] = "";
-    data = vector<uint8_t>(rawData, rawData + sizeof(rawData) - 1);
-    CRC32::Hash(data, res);
+    vector<uint8_t> data1;
+    CRC32::Hash(data1, res);
     EXPECT_STREQ(res.c_str(), "00000000");
 
     uint8_t rawData2[] = "Hello World!";
-    data = vector<uint8_t>(rawData2, rawData2 + sizeof(rawData2) - 1);
-    CRC32::Hash(data, res);
+    vector<uint8_t> data2(rawData2, rawData2 + sizeof(rawData2) - 1);
+    CRC32::Hash(data2, res);
     EXPECT_STREQ(res.c_str(), "1c291ca3");
 
     uint8_t rawData3[] = "If I sign myself Jean-Paul Sartre it is not the same thing as if I sign myself Jean-Paul Sartre, Nobel Prize winner.";
-    data = vector<uint8_t>(rawData3, rawData3 + sizeof(rawData3) - 1);
-    CRC32::Hash(data, res);
+    vector<uint8_t> data3(rawData3, rawData3 + sizeof(rawData3) - 1);
+    CRC32::Hash(data3, res);
     EXPECT_STREQ(res.c_str(), "75e33aef");
 
     uint8_t rawData4[] = "אורבס";
-    data = vector<uint8_t>(rawData4, rawData4 + sizeof(rawData4) - 1);
-    CRC32::Hash(data, res);
+    vector<uint8_t> data4(rawData4, rawData4 + sizeof(rawData4) - 1);
+    CRC32::Hash(data4, res);
     EXPECT_STREQ(res.c_str(), "426cf367");
 }
 
--- client/crypto-sdk/test/ripemd160-test.cpp
+++ client/crypto-sdk/test/ripemd160-test.cpp
@@ -10,26 +10,24 @@ using namespace Orbs;
 
 TEST(Hash, computes_binary_RIPEMD160_of_binary) {
     vector<uint8_t> res;
-    vector<uint8_t> data;
 
-    uint8_t rawData[] = "";
-    data = vector<uint8_t>(rawData, rawData + sizeof(rawData) - 1);
-    RIPEMD160::Hash(data, res);
+    vector<uint8_t> data1;
+    RIPEMD160::Hash(data1, res);
     EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("9c1185a5c5e9fc54612808977ee8f548b2258d31")));
 
     uint8_t rawData2[] = "Hello World!";
-    data = vector<uint8_t>(rawData2, rawData2 + sizeof(rawData2) - 1);
-    RIPEMD160::Hash(data, res);
+    vector<uint8_t> data2(rawData2, rawData2 + sizeof(rawData2) - 1);
+    RIPEMD160::Hash(data2, res);
     EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("8476ee4631b9b30ac2754b0ee0c47e161d3f724c")));
 
     uint8_t rawData3[] = "If I sign myself Jean-Paul Sartre it is not the same thing as if I sign myself Jean-Paul Sartre, Nobel Prize winner.";
-    data = vector<uint8_t>(rawData3, rawData3 + sizeof(rawData3) - 1);
-    RIPEMD160::Hash(data, res);
+    vector<uint8_t> data3(rawData3, rawData3 + sizeof(rawData3) - 1);
+    RIPEMD160::Hash(data3, res);
     EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("128c3544ca79c4c04cadd7eba5053eb6f5dadfdf")));
 
     uint8_t rawData4[] = "אורבס";
-    data = vector<uint8_t>(rawData4, rawData4 + sizeof(rawData4) - 1);
-    RIPEMD160::Hash(data, res);
+    vector<uint8_t> data4(rawData4, rawData4 + sizeof(rawData4) - 1);
+    RIPEMD160::Hash(data4, res);
     EXPECT_THAT(res, ElementsAreArray(Utils::Hex2Vec("00d229fc91599f0c5f676b4506073cd63f196986")));
 }
 
@@ -51,26 +49,24 @@ TEST(Hash, computes_binary_RIPEMD160_of_string) {
 
 TEST(Hash, computes_string_RIPEMD160_of_binary) {
     string res;
-    vector<uint8_t> data;
 
-    uint8_t rawData[] = "";
-    data = vector<uint8_t>(rawData, rawData + sizeof(rawData) - 1);
-    RIPEMD160::Hash(data, res);
+    vector<uint8_t> data1;
+    RIPEMD160::Hash(data1, res);
     EXPECT_STREQ(res.c_str(), "9c1185a5c5e9fc54612808977ee8f548b2258d31");
 
     uint8_t rawData2[] = "Hello World!";
-    data = vector<uint8_t>(rawData2, rawData2 + sizeof(rawData2) - 1);
-    RIPEMD160::Hash(data, res);
+    vector<uint8_t> data2(rawData2, rawData2 + sizeof(rawData2) - 1);
+    RIPEMD160::Hash(data2, res);
     EXPECT_STREQ(res.c_str(), "8476ee4631b9b30ac2754b0ee0c47e161d3f724c");
 
     uint8_t rawData3[] = "If I sign myself Jean-Paul Sartre it is not the same thing as if I sign myself Jean-Paul Sartre, Nobel Prize winner.";
-    data = vector<uint8_t>(rawData3, rawData3 + sizeof(rawData3) - 1);
-    RIPEMD160::Hash(data, res);
+    vector<uint8_t> data3(rawData3, rawData3 + sizeof(rawData3) - 1);
+    RIPEMD160::Hash(data3, res);
     EXPECT_STREQ(res.c_str(), "128c3544ca79c4c04cadd7eba5053eb6f5dadfdf");
 
     uint8_t rawData4[] = "אורבס";
-    data = vector<uint8_t>(rawData4, rawData4 + sizeof(rawData4) - 1);
-    RIPEMD160::Hash(data, res);
+    vector<uint8_t> data4(rawData4, rawData4 + sizeof(rawData4) - 1);
+    RIPEMD160::Hash(data4, res);
     EXPECT_STREQ(res.c_str(), "00d229fc91599f0c5f676b4506073cd63f196986");
 }
 
