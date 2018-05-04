747237c8bb4598f1a97c055aa526f537055d2967
Leonid Beder <leonid@orbs.com>
2018-05-04 19:59:59 +0300

    Add tests for compatibility with elliptic

--- client/client-sdk-javascript/test/ed25519key.spec.ts
+++ client/client-sdk-javascript/test/ed25519key.spec.ts
@@ -1,4 +1,5 @@
 import { expect } from "chai";
+import { eddsa } from "elliptic";
 
 import { ED25519Key } from "../src/index";
 
@@ -71,10 +72,49 @@ describe("an ed25519 key", () => {
   });
 
   it("can sign messages which can be verified externally", async () => {
-    expect(true).to.be.false;
+    const ec = new eddsa("ed25519");
+
+    const message1 = Buffer.from("Hello World!");
+    const publicKey1 = "b9a91acbf23c22123a8253cfc4325d7b4b7a620465c57f932c7943f60887308b";
+    const privateKey1 = "3f81e53116ee3f860c154d03b9cabf8af71d8beec210c535ed300c0aee5fcbe7";
+    const key1 = new ED25519Key(publicKey1, privateKey1);
+    const signature1: Buffer = key1.sign(message1);
+
+    const ecKey1 = ec.keyFromPublic(publicKey1);
+    expect(ecKey1.verify([...message1], [...signature1])).to.be.true;
+
+    const message2 = Buffer.from("If I sign myself Jean-Paul Sartre it is not the same thing as if I sign myself Jean - Paul Sartre, Nobel Prize winner.");
+    const publicKey2 = "f1e8935355da72309ffdfd4ec62b6f48abf8f690dc29abf77badc4b83596aab3";
+    const privateKey2 = "031f72d5fd7a518458f6e4d14fdcc8c28dedccef4b700f6351cd42ca84a7b935";
+    const key2 = new ED25519Key(publicKey2, privateKey2);
+    const signature2: Buffer = key2.sign(message2);
+
+    const ecKey2 = ec.keyFromPublic(publicKey2);
+    expect(ecKey2.verify([...message2], [...signature2])).to.be.true;
+
   });
 
   it("can verify externally signed messages", async () => {
-    expect(true).to.be.false;
+    const ec = new eddsa("ed25519");
+
+    const message1 = Buffer.from("Hello World!");
+    const publicKey1 = "b9a91acbf23c22123a8253cfc4325d7b4b7a620465c57f932c7943f60887308b";
+    const privateKey1 = "3f81e53116ee3f860c154d03b9cabf8af71d8beec210c535ed300c0aee5fcbe7";
+
+    const ecKey1 = ec.keyFromSecret(privateKey1);
+    const ecSignature1 = Buffer.from(ecKey1.sign([...message1]).toBytes());
+
+    const key1 = new ED25519Key(publicKey1);
+    expect(key1.verify(message1, ecSignature1)).to.be.true;
+
+    const message2 = Buffer.from("If I sign myself Jean-Paul Sartre it is not the same thing as if I sign myself Jean - Paul Sartre, Nobel Prize winner.");
+    const publicKey2 = "f1e8935355da72309ffdfd4ec62b6f48abf8f690dc29abf77badc4b83596aab3";
+    const privateKey2 = "031f72d5fd7a518458f6e4d14fdcc8c28dedccef4b700f6351cd42ca84a7b935";
+
+    const ecKey2 = ec.keyFromSecret(privateKey2);
+    const ecSignature2 = Buffer.from(ecKey2.sign([...message2]).toBytes());
+
+    const key2 = new ED25519Key(publicKey2);
+    expect(key2.verify(message2, ecSignature2)).to.be.true;
   });
 });
