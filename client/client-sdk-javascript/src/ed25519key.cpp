8eaae8ef9462d72f65f4edfa3d387fdf3479b086
Leonid Beder <leonid@orbs.com>
2018-05-04 17:10:19 +0300

    Add eddsa sign/verify capabilities

--- client/client-sdk-javascript/src/ed25519key.cpp
+++ client/client-sdk-javascript/src/ed25519key.cpp
@@ -13,6 +13,10 @@ napi_ref ED25519Key::constructor;
 ED25519Key::ED25519Key(const string &publicKey) : env_(nullptr), wrapper_(nullptr), key_(Orbs::Utils::Hex2Vec(publicKey)) {
 }
 
+ED25519Key::ED25519Key(const string &publicKey, const string &privateKey) : env_(nullptr), wrapper_(nullptr),
+    key_(Orbs::Utils::Hex2Vec(publicKey), Orbs::Utils::Hex2Vec(privateKey)) {
+}
+
 ED25519Key::ED25519Key() : env_(nullptr), wrapper_(nullptr) {
 }
 
@@ -29,7 +33,10 @@ void ED25519Key::Destructor(napi_env env, void *nativeObject, void * /*finalize_
 napi_value ED25519Key::Init(napi_env env, napi_value exports) {
     napi_status status;
     napi_property_descriptor properties[] = {
-        { "publicKey", 0, 0, GetPublicKey, nullptr, 0, napi_default, 0 }
+        { "publicKey", 0, 0, GetPublicKey, nullptr, 0, napi_default, 0 },
+        { "hasPrivateKey", 0, 0, HasPrivateKey, nullptr, 0, napi_default, 0 },
+        DECLARE_NAPI_METHOD("sign", Sign),
+        DECLARE_NAPI_METHOD("verify", Verify),
     };
 
     napi_value cons;
@@ -55,8 +62,8 @@ napi_value ED25519Key::New(napi_env env, napi_callback_info info) {
 
     if (is_constructor) {
         // Invoked as constructor: `new ED25519Key(...)`
-        size_t argc = 1;
-        napi_value args[1];
+        size_t argc = 2;
+        napi_value args[2];
         napi_value jsthis;
         status = napi_get_cb_info(env, info, &argc, args, &jsthis, nullptr);
         assert(status == napi_ok);
@@ -68,9 +75,19 @@ napi_value ED25519Key::New(napi_env env, napi_callback_info info) {
             status = napi_get_value_string_utf8(env, args[0], nullptr, 0, &publicKeyLength);
             assert(status == napi_ok);
             char publicKey[publicKeyLength + 1];
-            napi_get_value_string_utf8(env, args[0], publicKey, publicKeyLength + 1, 0);
+            napi_get_value_string_utf8(env, args[0], publicKey, publicKeyLength + 1, nullptr);
 
-            obj = new ED25519Key(publicKey);
+            if (argc == 1) {
+                obj = new ED25519Key(publicKey);
+            } else {
+                size_t privateKeyLength;
+                status = napi_get_value_string_utf8(env, args[1], nullptr, 0, &privateKeyLength);
+                assert(status == napi_ok);
+                char privateKey[privateKeyLength + 1];
+                napi_get_value_string_utf8(env, args[1], privateKey, privateKeyLength + 1, nullptr);
+
+                obj = new ED25519Key(publicKey, privateKey);
+            }
         } else {
             obj = new ED25519Key();
         }
@@ -116,6 +133,122 @@ napi_value ED25519Key::GetPublicKey(napi_env env, napi_callback_info info) {
     const string str(Orbs::Utils::Vec2Hex(obj->key_.GetPublicKey()));
     napi_value res;
     status = napi_create_string_utf8(env, str.c_str(), str.length(), &res);
+    assert(status == napi_ok);
+
+    return res;
+}
+
+napi_value ED25519Key::HasPrivateKey(napi_env env, napi_callback_info info) {
+    napi_status status;
+
+    napi_value jsthis;
+    status = napi_get_cb_info(env, info, nullptr, nullptr, &jsthis, nullptr);
+    assert(status == napi_ok);
+
+    ED25519Key *obj;
+    status = napi_unwrap(env, jsthis, reinterpret_cast<void **>(&obj));
+    assert(status == napi_ok);
+
+    napi_value res;
+    status = napi_get_boolean(env, obj->key_.HasPrivateKey(), &res);
+    assert(status == napi_ok);
+
+    return res;
+}
+
+napi_value ED25519Key::Sign(napi_env env, napi_callback_info info) {
+    napi_status status;
+
+    napi_value jsthis;
+    size_t argc = 1;
+    napi_value args[1];
+    status = napi_get_cb_info(env, info, &argc, args, &jsthis, nullptr);
+    assert(status == napi_ok);
+
+    if (argc < 1) {
+        napi_throw_type_error(env, nullptr, "Wrong number of arguments!");
+
+        return nullptr;
+    }
+
+    bool isValue1Buffer = false;
+    status = napi_is_buffer(env, args[0], &isValue1Buffer);
+    assert(status == napi_ok);
+
+    if (!isValue1Buffer) {
+        napi_throw_type_error(env, nullptr, "Wrong arguments!");
+
+        return nullptr;
+    }
+
+    size_t messageLength;
+    void *message;
+    status = napi_get_buffer_info(env, args[0], &message, &messageLength);
+    assert(status == napi_ok);
+
+    ED25519Key *obj;
+    status = napi_unwrap(env, jsthis, reinterpret_cast<void **>(&obj));
+    assert(status == napi_ok);
+
+    const vector<uint8_t> rawMessage(static_cast<uint8_t *>(message), static_cast<uint8_t *>(message) + messageLength);
+    const vector<uint8_t> rawSignature(obj->key_.Sign(rawMessage));
+
+    napi_value res;
+    status = napi_create_buffer_copy(env, rawSignature.size(),  &rawSignature[0], nullptr, &res);
+    assert(status == napi_ok);
+
+    return res;
+}
+
+napi_value ED25519Key::Verify(napi_env env, napi_callback_info info) {
+    napi_status status;
+
+    napi_value jsthis;
+    size_t argc = 2;
+    napi_value args[2];
+    status = napi_get_cb_info(env, info, &argc, args, &jsthis, nullptr);
+    assert(status == napi_ok);
+
+    if (argc < 2) {
+        napi_throw_type_error(env, nullptr, "Wrong number of arguments!");
+
+        return nullptr;
+    }
+
+    bool isValue1Buffer = false;
+    status = napi_is_buffer(env, args[0], &isValue1Buffer);
+    assert(status == napi_ok);
+
+    bool isValue2Buffer = false;
+    status = napi_is_buffer(env, args[0], &isValue2Buffer);
+    assert(status == napi_ok);
+
+     if (!isValue1Buffer || !isValue2Buffer) {
+        napi_throw_type_error(env, nullptr, "Wrong arguments!");
+
+        return nullptr;
+    }
+
+    size_t messageLength;
+    void *message;
+    status = napi_get_buffer_info(env, args[0], &message, &messageLength);
+    assert(status == napi_ok);
+
+    size_t signatureLength;
+    void *signature;
+    status = napi_get_buffer_info(env, args[1], &signature, &signatureLength);
+    assert(status == napi_ok);
+
+    ED25519Key *obj;
+    status = napi_unwrap(env, jsthis, reinterpret_cast<void **>(&obj));
+    assert(status == napi_ok);
+
+    const vector<uint8_t> rawMessage(static_cast<uint8_t *>(message), static_cast<uint8_t *>(message) + messageLength);
+    const vector<uint8_t> rawSignature(static_cast<uint8_t *>(signature), static_cast<uint8_t *>(signature) + signatureLength);
+
+    napi_value res;
+    status = napi_get_boolean(env, obj->key_.Verify(rawMessage, rawSignature), &res);
+    assert(status == napi_ok);
 
     return res;
 }
--- client/client-sdk-javascript/test/ed25519key.spec.ts
+++ client/client-sdk-javascript/test/ed25519key.spec.ts
@@ -4,15 +4,31 @@ import { ED25519Key } from "../src/index";
 
 describe("an ed25519 key", () => {
   it("is properly initialized by a public key", async () => {
-    const publicKey = "8d41d055d00459be37f749da2caf87bd4ced6fafa335b1f2142e0f44501b2c65";
-    const key1 = new ED25519Key(publicKey);
+    const publicKey1 = "8d41d055d00459be37f749da2caf87bd4ced6fafa335b1f2142e0f44501b2c65";
+    const key1 = new ED25519Key(publicKey1);
 
-    expect(key1.publicKey).to.equal(publicKey);
+    expect(key1.publicKey).to.equal(publicKey1);
+    expect(key1.hasPrivateKey).to.equal(false);
 
     const publicKey2 = "7a463487bb0eb584dabccd52398506b4a2dd432503cc6b7b582f87832ad104e6";
     const key2 = new ED25519Key(publicKey2);
+    expect(key2.hasPrivateKey).to.equal(false);
 
     expect(key2.publicKey).to.equal(publicKey2);
+  });
+
+  it("is properly initialized by a public key and private key", async () => {
+    const publicKey1 = "b9a91acbf23c22123a8253cfc4325d7b4b7a620465c57f932c7943f60887308b";
+    const privateKey1 = "3f81e53116ee3f860c154d03b9cabf8af71d8beec210c535ed300c0aee5fcbe7";
+    const key1 = new ED25519Key(publicKey1, privateKey1);
+    expect(key1.publicKey).to.equal(publicKey1);
+    expect(key1.hasPrivateKey).to.equal(true);
+
+    const publicKey2 = "f1e8935355da72309ffdfd4ec62b6f48abf8f690dc29abf77badc4b83596aab3";
+    const privateKey2 = "031f72d5fd7a518458f6e4d14fdcc8c28dedccef4b700f6351cd42ca84a7b935";
+    const key2 = new ED25519Key(publicKey2, privateKey2);
+    expect(key2.publicKey).to.equal(publicKey2);
+    expect(key2.hasPrivateKey).to.equal(true);
   });
 
   it("is randomly generated", async () => {
@@ -21,15 +37,37 @@ describe("an ed25519 key", () => {
     const key3 = new ED25519Key();
 
     expect(key1.publicKey).to.have.length(64);
+    expect(key1.hasPrivateKey).to.equal(true);
     expect(key2.publicKey).to.have.length(64);
+    expect(key2.hasPrivateKey).to.equal(true);
     expect(key3.publicKey).to.have.length(64);
+    expect(key3.hasPrivateKey).to.equal(true);
 
     expect(key1.publicKey).not.to.be.equal(key2.publicKey);
     expect(key2.publicKey).not.to.be.equal(key3.publicKey);
   });
 
   it("can sign and verify messages", async () => {
-    expect(true).to.be.false;
+    const message1 = Buffer.from("Hello World!");
+    const publicKey1 = "b9a91acbf23c22123a8253cfc4325d7b4b7a620465c57f932c7943f60887308b";
+    const privateKey1 = "3f81e53116ee3f860c154d03b9cabf8af71d8beec210c535ed300c0aee5fcbe7";
+    const key1 = new ED25519Key(publicKey1, privateKey1);
+    const signature1 = key1.sign(message1);
+    expect(key1.verify(message1, signature1)).to.be.true;
+
+    const message2 = Buffer.from("If I sign myself Jean-Paul Sartre it is not the same thing as if I sign myself Jean - Paul Sartre, Nobel Prize winner.");
+    const publicKey2 = "f1e8935355da72309ffdfd4ec62b6f48abf8f690dc29abf77badc4b83596aab3";
+    const privateKey2 = "031f72d5fd7a518458f6e4d14fdcc8c28dedccef4b700f6351cd42ca84a7b935";
+    const key2 = new ED25519Key(publicKey2, privateKey2);
+    const signature2 = key2.sign(message2);
+    expect(key2.verify(message2, signature2)).to.be.true;
+
+    expect(key1.verify(message2, signature1)).to.be.false;
+    expect(key1.verify(message1, signature2)).to.be.false;
+    expect(key1.verify(message2, signature2)).to.be.false;
+    expect(key2.verify(message2, signature1)).to.be.false;
+    expect(key2.verify(message1, signature2)).to.be.false;
+    expect(key2.verify(message1, signature1)).to.be.false;
   });
 
   it("can sign messages which can be verified externally", async () => {
