a0428c31b6a0b6994713902b7332f3d3e0deeebf
Ido Zilberberg <izilberberg@gmail.com>
2018-05-06 10:59:34 +0300

    Removed atom, alfred; better explanation on rerunning script

--- orbs-team-member-bootstrap.sh
+++ orbs-team-member-bootstrap.sh
@@ -12,7 +12,11 @@
 DEV_HOME=${HOME}/dev/orbs
 NODE_VER="v9.11.1"
 
-CASK_PACKAGES="google-chrome visual-studio-code iterm2 atom slack java8 docker sourcetree spectacle alfred"
+NVM_INSTALL_URL="https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh"
+BREW_INSTALL_URL="https://raw.githubusercontent.com/Homebrew/install/master/install"
+OH_MY_ZSH_URL="https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh"
+
+CASK_PACKAGES="google-chrome visual-studio-code iterm2 slack java8 docker sourcetree spectacle"
 PACKAGES="typescript yarn"
 
 FAILED_PACKAGES=""
@@ -22,21 +26,21 @@ ORBS_NETWORK_HOME=${DEV_HOME}/orbs-network
 
 exit_with_message() 
 {  
-  echo "Failed: ${1-}"; 
+  echo "Failed: ${1-}";
   exit 1;
 }
 
 install_nvm_and_node()
 {
   echo "Installing NVM ..."
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+  curl -o- ${NVM_INSTALL_URL} | bash
 
   # Allow nvm to run in the current session (so user doens't have to stop, restart session and rerun)
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-  [ "$(command -v nvm)" != "nvm" ] && exit_on_fail 1 "NVM failed to install, or shell needs to be restarted. Please restart shell and run this script again.."
+  [ "$(command -v nvm)" != "nvm" ] && exit_with_message "NVM failed to install, or shell needs to be restarted. Please restart shell and run this script again.."
 
   echo "nvm installed successfully, installing Node version ${NODE_VER} ..."
 
@@ -47,7 +51,7 @@ install_brew_and_cask()
 {
   if [ ! -f /usr/local/bin/brew ] ; then
     echo "Installing brew..."
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /usr/bin/ruby -e "$(curl -fsSL ${BREW_INSTALL_URL})"
     [ $? -ne 0 ] && exit_with_message "Failed to install brew, cannot continue"
   else
     echo "Brew already installed, skipping"
@@ -109,33 +113,34 @@ run_build()
   ./docker/build-server-base.sh && ./docker-build.sh
   ./docker/build-sdk-base.sh && ./docker/build-sdk.sh
   ./docker/build-sdk-base.sh && ./docker/build-e2e.sh
-
 }
 
-// ==================== START ====================
+# ==================== START ====================
 
 echo
 echo "Welcome to Orbs, dear ${USER}!"
 echo
 echo "This script installs software that you will use as a software engineer at Orbs."
-echo "It will first install software, and then ask you if you would like to run build (it takes several minutes to run)."
+echo "After installing software, you will be asked if you would like to run build (it takes several minutes to run)."
+echo "If you want to run build later, you can answer 'n', then rerun the script and answer 'y'."
 echo "During installation, the script you will occasionally be asked for your MacOS password."
-echo "P.S. It is safe to run this script multiple times."
 echo
-echo "The script will install Node.js version ${NODE_VER}."
+echo "It is safe to run this script multiple times."
+echo
+echo "Node.js version to install: ${NODE_VER}."
 echo "If you need to change that, quit now (^C) and change NODE_VER variable at the top of the script."
 echo
 read -rsn1 -p"Press any key to begin";echo
 
 echo "Installing Oh My Zsh (alternative shell) ..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL ${OH_MY_ZSH_URL})"
 
 install_nvm_and_node
 install_brew_and_cask
 install_brew_packages
 install_cask_packages
 
-# Don't install android for now
+# This is not called by default as not everyone needs it. Uncomment and rerun to install it.
 # install_android 
 
 echo "Running post-installation actions..."
@@ -146,16 +151,18 @@ fi
 
 # source ~/.zshrc
 
-echo "Node version: $(node -v)"
-echo |"NPM version: $(npm -v)"
+echo "Installed Node version: $(node -v)"
+echo "Installed NPM version: $(npm -v)"
 
 if [ -n ${FAILED_PACKAGES} ] ; then
-  echo "The following packages failed to install, try to install them manually: ${FAILED_PACKAGES}"
+  echo "The following packages failed to install: ${FAILED_PACKAGES}"
+  echo "Try to install manually or rerun the script."
 fi
 
 
 
 echo "The script can now run build. If you choose to run it, it will run and then exit. Otherwise it will exit now."
+ehco "If you wish to run build later, rerun the script and answer 'y'."
 echo
 read -r -p "Would you like to run build now? [y/N] " response
 if [ "${response-}" == "Y" ] || [ "${response-}" == "y" ] ; then
@@ -170,13 +177,13 @@ fi
 
 echo
 
-echo "Nice tip: if you want to set your key repeat rate faster than System Preferences would let you, run the following, then logout and log back in:"
+echo "Productivity tip: to set your key repeat rate faster than what System Preferences UI would let you, run the following, then logout and log back in:"
 echo "defaults write NSGlobalDomain KeyRepeat -int 1"
 echo "defaults write NSGlobalDomain InitialKeyRepeat -int 10"
 
 echo
 echo "Installation complete, thank you for your patience."
-echo "Please restart the current session for some of the changes to take effect."
+echo "Please restart the current session for changes to take effect."
 echo
 echo
 
