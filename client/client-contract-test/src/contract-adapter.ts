1c110356915d769f515079dd99a384c0ba91b8ae
idandush <idanofrat@gmail.com>
2018-05-13 16:03:47 +0300

    test as a feature

--- client/client-contract-test/src/contract-adapter.ts
+++ client/client-contract-test/src/contract-adapter.ts
@@ -11,7 +11,8 @@ export interface OrbsContractAdapter {
     getCallObject(): OrbsAPICallContractRequest;
 }
 
-export function testContract(makeContract: () => OrbsContractAdapter) {
+export function testContract(makeContract: () => OrbsContractAdapter, testSignature = true) {
+    const ifit = testSignature ? it : xit;
 
     describe("calls the connector interface with the correct inputs when", async function () {
 
@@ -20,14 +21,18 @@ export function testContract(makeContract: () => OrbsContractAdapter) {
         expect(sendTransactionObject).to.have.property("payload").that.is.eql(expectedSendTransactionRequest.payload);
         expect(sendTransactionObject).to.have.property("header").that.has.property("senderAddressBase58").that.is.eql(expectedSendTransactionRequest.header.senderAddressBase58);
         expect(sendTransactionObject).to.have.property("header").that.has.property("contractAddressBase58").that.is.eql(expectedSendTransactionRequest.header.contractAddressBase58);
-        expect(sendTransactionObject).to.have.property("signatureData").that.has.property("publicKeyHex").that.is.eql(expectedSendTransactionRequest.signatureData.publicKeyHex);
-        expect(sendTransactionObject).to.have.property("signatureData").that.has.property("signatureHex").that.is.eql(expectedSendTransactionRequest.signatureData.signatureHex);
         expect(sendTransactionObject).to.have.property("header").that.has.property("timestamp").that.is.a("string");
         const now = Date.now();
         // testing that timestamp is less than a couple of seconds old and not in the future
         const timestamp = Number(sendTransactionObject.header.timestamp);
         expect(timestamp).to.be.above(now - (30 * 1000));
         expect(timestamp).to.be.below(now + (10 * 1000));
+      });
+
+      ifit("getSendTranscationObject() is called", async () => {
+        const sendTransactionObject = await makeContract().getSendTranscationObject();
+        expect(sendTransactionObject).to.have.property("signatureData").that.has.property("publicKeyHex").that.is.eql(expectedSendTransactionRequest.signatureData.publicKeyHex);
+        expect(sendTransactionObject).to.have.property("signatureData").that.has.property("signatureHex").that.is.eql(expectedSendTransactionRequest.signatureData.signatureHex);
       });
 
       it("getCallObject() is called", async () => {
