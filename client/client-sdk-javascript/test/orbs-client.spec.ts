7977ebe197296a036fce2a35cd5ad6f02e95991d
idandush <idanofrat@gmail.com>
2018-05-15 11:16:03 +0300

    minor fixes pr pr comments

---
+++ client/client-sdk-javascript/test/orbs-client.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import { eddsa } from "elliptic";
+
+import { ED25519Key } from "../src/index";
+
+describe("Client", () => {
+  it("signs a transaction with the signature provided by the key pair object", () => {
+
+  });
+});
