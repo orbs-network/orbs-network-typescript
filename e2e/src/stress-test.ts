56d6bd330a7aae49e9a891dd926115071eaac533
Eran Kirshenboim <eran.kirshenboim@gmail.com>
2018-05-16 20:51:51 +0300

    fixed stress-e2e

--- e2e/src/stress-test.ts
+++ e2e/src/stress-test.ts
@@ -13,10 +13,16 @@ chai.use(ChaiBarsPlugin);
 
 const testConfig = loadDefaultTestConfig();
 const { API_ENDPOINT } = process.env;
+const generateAddress = (keyPair: ED25519Key): Address => {
+  const address = new Address(keyPair.publicKey, testConfig.virtualChainId, Address.TEST_NETWORK_ID);
 
-async function aFooBarAccountWith(input: { keyPair: ED25519Key, amountOfBars: number }) {
-  const senderAddress = new Address(input.keyPair.publicKey, testConfig.virtualChainId, Address.TEST_NETWORK_ID);
-  const orbsClient = new OrbsClient(API_ENDPOINT, senderAddress, input.keyPair);
+  return address;
+};
+
+async function aFooBarAccountWith(input: { amountOfBars: number }) {
+  const keyPair = new ED25519Key();
+  const senderAddress = generateAddress(keyPair);
+  const orbsClient = new OrbsClient(testConfig.apiEndpoint, senderAddress, keyPair);
   const contractAdapter = new OrbsContract(orbsClient, "foobar");
   const account = new FooBarAccount(senderAddress.toString(), contractAdapter);
 
@@ -28,8 +34,8 @@ async function aFooBarAccountWith(input: { keyPair: ED25519Key, amountOfBars: nu
 async function createAccounts(input: { seed: number, numberOfAccounts: number }): Promise<FooBarAccount[]> {
   return Promise.all(_.range(input.numberOfAccounts).map((num) => {
     const amountOfBars = num + 10;
-    const keyPair = new ED25519Key();
-    return aFooBarAccountWith({ keyPair, amountOfBars });
+
+    return aFooBarAccountWith({ amountOfBars });
   }));
 }
 
@@ -51,6 +57,35 @@ describe("test multiple transactions", async function () {
     const accounts = await createAccounts({ seed: seed, numberOfAccounts: testConfig.stressTest.accounts });
 
     await Promise.all(accounts.map((account, num) => expect(account).to.have.bars(10 + num)));
+
+    await Promise.all(accounts.map(async (account, num) => {
+      console.log(`Account ${account.address} has balance ${await account.getBalance()}`);
+    }));
+
+    for (let index = 0; index < 10; index++) {
+      await Promise.all(accounts.map((account, num) => {
+        const isLast = num + 1 === testConfig.stressTest.accounts;
+        const recipient = accounts[isLast ? 0 : num + 1];
+        const amount = num + 1;
+
+        console.log(`Sending ${amount} bar from ${account.address} to ${recipient.address}`);
+
+        return account.transfer({ to: recipient.address, amountOfBars: amount });
+      }));
+
+      await Promise.all(accounts.map((account, num) => {
+        const isFirst = num === 0;
+        const recipient = accounts[isFirst ? testConfig.stressTest.accounts - 1 : num - 1];
+        const amount = isFirst ? testConfig.stressTest.accounts - 1 : num - 1;
+
+        console.log(`Sending ${amount} bar from ${account.address} to ${recipient.address}`);
+
+        return account.transfer({ to: recipient.address, amountOfBars: amount });
+      }));
+    }
+
+
+
 
     await Promise.all(accounts.map((account, num) => {
       const isLast = num + 1 === testConfig.stressTest.accounts;
