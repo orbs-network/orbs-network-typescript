cf4a05cdf5bfaa82c9d1a6cde4c4449bd7a7e248
Leonid Beder <leonid@orbs.com>
2018-05-19 03:22:14 +0300

    Style

--- client/crypto-sdk-python/lib/ed25519key.cpp
+++ client/crypto-sdk-python/lib/ed25519key.cpp
@@ -11,8 +11,12 @@ void ExportED25519() {
     class_<ED25519Key, boost::noncopyable>("ED25519Key", init<>())
         .def(init<string>())
         .def(init<string, string>())
-        .add_property("public_key", +[](const ED25519Key &k) { return Utils::Vec2Hex(k.GetPublicKey()); })
-        .add_property("unsafe_private_key", +[](const ED25519Key &k) { return Utils::Vec2Hex(k.GetPrivateKeyUnsafe()); })
+        .add_property("public_key", +[](const ED25519Key &k) {
+            return Utils::Vec2Hex(k.GetPublicKey());
+        })
+        .add_property("unsafe_private_key", +[](const ED25519Key &k) {
+            return Utils::Vec2Hex(k.GetPrivateKeyUnsafe());
+        })
         .add_property("has_private_key", &ED25519Key::HasPrivateKey)
         .def("sign", +[](const ED25519Key &k, const string &message) {
             const vector<uint8_t> rawMessage(message.cbegin(), message.cend());
