05c9ab628c833f28a4d50d95beecd03638decc06
idandush <idanofrat@gmail.com>
2018-05-21 14:43:56 +0300

    Subscription Manager support subscription rates & profiles

--- projects/libs/core-library-typescript/src/subscription-manager/subscription-manager.ts
+++ projects/libs/core-library-typescript/src/subscription-manager/subscription-manager.ts
@@ -3,8 +3,18 @@ import { types } from "../common-library/types";
 
 import { ERCBillingContractProxy, Subscription } from "./erc-billing-contract-proxy";
 
+export interface MonthlySubscriptionRateData {
+  expiresAt?: Date;
+  rate: number;
+}
+
+export interface SubscriptionProfiles {
+  [profile: string]: MonthlySubscriptionRateData[];
+}
+
 export class SubscriptionManagerConfiguration {
   ethereumContractAddress: string;
+  subscriptionProfiles: SubscriptionProfiles;
 }
 
 export class SubscriptionManager {
@@ -16,7 +26,58 @@ export class SubscriptionManager {
     this.contractProxy = new ERCBillingContractProxy(sidechainConnector, this.config.ethereumContractAddress);
   }
 
-  async getSubscriptionStatus(subscriptionKey: string) {
-    return await this.contractProxy.getSubscription(subscriptionKey);
+  async isSubscriptionValid(subscriptionKey: string): Promise<boolean> {
+    const subscriptionData =  await this.contractProxy.getSubscriptionData(subscriptionKey);
+
+    const now = new Date();
+
+    const rateData = this.getCurrentSubscriptionRateByProfile(subscriptionData.profile, now);
+
+    if (rateData == undefined) {
+      logger.info(`Could not find rate settings for subscription profile ${subscriptionData.profile}`);
+      return false;
+    }
+
+    const beginningOfThisMonth = new Date(`${now.toISOString().substr(0, 7)}-01`).getTime();
+
+    let minimumRequiredFee: number;
+
+    if (subscriptionData.startTime <= beginningOfThisMonth) {
+      minimumRequiredFee = rateData.rate;
+    } else {
+      const numOfDaysThisMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+      const endOfThisMonth = new Date(`${now.toISOString().substr(0, 7)}-${numOfDaysThisMonth}`).getTime();
+
+      const subscriptionDurationInMilliseconds = endOfThisMonth - subscriptionData.startTime;
+      const numOfMillisecondsThisMonth = endOfThisMonth - beginningOfThisMonth;
+
+      minimumRequiredFee = rateData.rate * (subscriptionDurationInMilliseconds / numOfMillisecondsThisMonth);
+    }
+
+    if (subscriptionData.tokens <= minimumRequiredFee) {
+      logger.info(`Subscription fee is not sufficient (${subscriptionData.tokens} < ${minimumRequiredFee})`);
+      return false;
+    }
+
+    return true;
+  }
+
+  getCurrentSubscriptionRateByProfile(profile: string, date: Date): MonthlySubscriptionRateData {
+    const profileRates: MonthlySubscriptionRateData[] = this.config.subscriptionProfiles[profile];
+    if (profileRates != undefined) {
+      let nonExpiredRates: MonthlySubscriptionRateData[];
+      nonExpiredRates = profileRates.filter(rateData => (rateData.expiresAt == undefined) || (rateData.expiresAt > date));
+      if (nonExpiredRates.length > 0 ) {
+        return nonExpiredRates.sort((a, b) => {
+          if (a.expiresAt == undefined) {
+            return 1; // a >= b
+          } else if (b.expiresAt == undefined) {
+            return -1; // b >= a
+          } else {
+            return a.expiresAt.getTime() - b.expiresAt.getTime();
+          }
+        })[0];
+      }
+    }
   }
 }
---
+++ projects/libs/core-library-typescript/test/subscription-manager/subscription-manager.spec.ts
@@ -0,0 +1,119 @@
+import { SubscriptionManager, types } from "../../src";
+import { stubInterface } from "ts-sinon";
+import { SidechainConnector } from "orbs-interfaces";
+import * as chai from "chai";
+import { expect } from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+import * as sinonChai from "sinon-chai";
+import * as sinon from "sinon";
+
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+
+describe("isSubscriptionValid()", () => {
+  let subscriptionManager: SubscriptionManager;
+  const sidechainConnector = stubInterface<types.SidechainConnectorClient>();
+  const subscriptionKey = "0x0202020202020202020202020202020202020202020202020202020202020202";
+  let clock: sinon.SinonFakeTimers;
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers(new Date("2018-05-20"));
+    subscriptionManager = new SubscriptionManager(sidechainConnector, {
+      ethereumContractAddress: "0x0101010101010101010101010101010101010101010101010101010101010101",
+      subscriptionProfiles: {
+        "testProfile": [{
+          rate: 1000,
+          expiresAt: new Date("2018-06-01")
+        }, {
+          rate: 2000,
+          expiresAt: undefined
+        }]
+      }
+    });
+  });
+
+  it("returns true for subscription with sufficient fees", async () => {
+    const result = {
+      id: subscriptionKey,
+      profile: "testProfile",
+      startTime: (new Date("2018-01-01").getTime()),
+      tokens: 1100
+    };
+    (<sinon.SinonStub>sidechainConnector.callEthereumContract).returns(<types.CallEthereumContractOutput>{
+      resultJson: JSON.stringify(result)
+    });
+    return expect(subscriptionManager.isSubscriptionValid(subscriptionKey)).to.eventually.be.true;
+  });
+
+  it("returns true for subscription with sufficient fees that started in the middle of this month", async () => {
+    const result = {
+      id: subscriptionKey,
+      profile: "testProfile",
+      startTime: (new Date("2018-05-15").getTime()),
+      tokens: 600
+    };
+    (<sinon.SinonStub>sidechainConnector.callEthereumContract).returns(<types.CallEthereumContractOutput>{
+      resultJson: JSON.stringify(result)
+    });
+    return expect(subscriptionManager.isSubscriptionValid(subscriptionKey)).to.eventually.be.true;
+  });
+
+
+  it("returns true for subscription with insufficient fees", async () => {
+    const result = {
+      id: subscriptionKey,
+      profile: "testProfile",
+      startTime: (new Date("2018-01-01").getTime()),
+      tokens: 900
+    };
+    (<sinon.SinonStub>sidechainConnector.callEthereumContract).returns(<types.CallEthereumContractOutput>{
+      resultJson: JSON.stringify(result)
+    });
+    return expect(subscriptionManager.isSubscriptionValid(subscriptionKey)).to.eventually.be.false;
+  });
+
+  it("returns true for subscription with insufficient fees that started in the middle of this month", async () => {
+    const result = {
+      id: subscriptionKey,
+      profile: "testProfile",
+      startTime: (new Date("2018-05-15").getTime()),
+      tokens: 500
+    };
+    (<sinon.SinonStub>sidechainConnector.callEthereumContract).returns(<types.CallEthereumContractOutput>{
+      resultJson: JSON.stringify(result)
+    });
+    return expect(subscriptionManager.isSubscriptionValid(subscriptionKey)).to.eventually.be.false;
+  });
+
+  it("returns false if the profile is not configured", async () => {
+    const result = {
+      id: subscriptionKey,
+      profile: "nonConfiguredProfile",
+      startTime: (new Date("2018-01-01").getTime()),
+      tokens: 100000
+    };
+    (<sinon.SinonStub>sidechainConnector.callEthereumContract).returns(<types.CallEthereumContractOutput>{
+      resultJson: JSON.stringify(result)
+    });
+    return expect(subscriptionManager.isSubscriptionValid(subscriptionKey)).to.eventually.be.false;
+  });
+
+  it("returns false if the rate went up but the subscription fee has not updated accordingly", async () => {
+    clock.setSystemTime(new Date("2018-07-05"));
+    const result = {
+      id: subscriptionKey,
+      profile: "testProfile",
+      startTime: (new Date("2018-01-01").getTime()),
+      tokens: 1100
+    };
+    (<sinon.SinonStub>sidechainConnector.callEthereumContract).returns(<types.CallEthereumContractOutput>{
+      resultJson: JSON.stringify(result)
+    });
+    return expect(subscriptionManager.isSubscriptionValid(subscriptionKey)).to.eventually.be.false;
+  });
+
+  afterEach(() => {
+    clock.restore();
+  });
+});
