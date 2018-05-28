3e5c579e2f443ff7a06206d87c20514fe93ef702
Eran Kirshenboim <eran.kirshenboim@gmail.com>
2018-05-28 12:43:14 +0300

    allow 1 node to be out of sync before terminating stub consensus

--- projects/libs/core-library-typescript/src/consensus/stub-consensus.ts
+++ projects/libs/core-library-typescript/src/consensus/stub-consensus.ts
@@ -73,20 +73,25 @@ export class StubConsensus extends BaseConsensus {
       logger.debug(`Stub consensus leader heartbeat - my last block is ${JSON.stringify(myLastCommittedBlock)}`);
 
       // check if all other nodes are synchronized with me
-      let areAllOtherNodesSyncedWithMe = true;
-      if (this.lastBlockHeightByNodeName.size != this.config.clusterSize - 1) {
+      let areEnoughNodesSyncedWithMe = true;
+      if (this.lastBlockHeightByNodeName.size < this.config.clusterSize - this.config.acceptableUnsyncedNodes - 1) {
         logger.debug(`Stub consensus leader heartbeat - missing some other nodes since we just see ${this.lastBlockHeightByNodeName.size}`);
-        areAllOtherNodesSyncedWithMe = false;
+        areEnoughNodesSyncedWithMe = false;
       } else {
+        let howManyNodesAreNotSynced = 0;
         for (const [nodeName, lastBlockHeight] of this.lastBlockHeightByNodeName) {
           if (lastBlockHeight != myLastCommittedBlockHeight) {
             logger.debug(`Stub consensus leader heartbeat - last block of ${nodeName} is ${lastBlockHeight} while mine is ${myLastCommittedBlockHeight}`);
-            areAllOtherNodesSyncedWithMe = false;
+            howManyNodesAreNotSynced++;
+          }
+          if (howManyNodesAreNotSynced > this.config.acceptableUnsyncedNodes) {
+            logger.debug(`Stub consensus leader heartbeat - There are ${howManyNodesAreNotSynced} out of sync.`);
+            areEnoughNodesSyncedWithMe = false;
           }
         }
       }
 
-      if (areAllOtherNodesSyncedWithMe) {
+      if (areEnoughNodesSyncedWithMe) {
         // they are synchronized, let's create a new block
         logger.debug(`Stub consensus leader heartbeat - everybody is synchronized, next block please`);
         this.blockBuilder.start();
