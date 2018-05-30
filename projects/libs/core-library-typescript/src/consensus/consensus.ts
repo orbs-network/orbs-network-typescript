c56a8e689c475a98eed5d245f84a8a6746954305
Ido Zilberberg <idoz@orbs.com>
2018-05-30 12:38:29 +0300

    Consensus startup check

--- projects/libs/core-library-typescript/src/consensus/consensus.ts
+++ projects/libs/core-library-typescript/src/consensus/consensus.ts
@@ -6,16 +6,19 @@ import { Gossip } from "../gossip";
 import { RaftConsensusConfig, BaseConsensus } from "./base-consensus";
 import { RaftConsensus } from "./raft-consensus";
 import { StubConsensus } from "./stub-consensus";
+import { StartupCheck } from "../common-library/startup-check";
+import { StartupStatus, STARTUP_STATUS } from "../common-library/startup-status";
 
-export class Consensus {
+export class Consensus implements StartupCheck {
+  private COMPONENT_NAME = "consensus";
   private actualConsensus: BaseConsensus;
   private config: RaftConsensusConfig;
 
   constructor(
     config: RaftConsensusConfig, gossip: types.GossipClient,
     virtualMachine: types.VirtualMachineClient, blockStorage: types.BlockStorageClient,
-     transactionPool: types.TransactionPoolClient) {
-      this.config = config;
+    transactionPool: types.TransactionPoolClient) {
+    this.config = config;
 
     if (config.algorithm.toLowerCase() === "stub") {
       this.actualConsensus = new StubConsensus(config, gossip, blockStorage, transactionPool, virtualMachine);
@@ -35,4 +38,15 @@ export class Consensus {
   async gossipMessageReceived(fromAddress: string, messageType: string, message: any) {
     await this.actualConsensus.onMessageReceived(fromAddress, messageType, message);
   }
+
+  public async startupCheck(): Promise<StartupStatus> {
+    if (!this.actualConsensus) {
+      return { name: this.COMPONENT_NAME, status: STARTUP_STATUS.FAIL, message: "Missing actualConsensus" };
+    }
+
+    return { name: this.COMPONENT_NAME, status: STARTUP_STATUS.OK };
+
+    // return this.consensus.startupCheck();
+  }
+
 }
