cf07383ca68b5b9d9ea5a1a8d6420718c34998d3
Jonathan Levison <jonathanl@orbs.com>
2018-06-17 20:03:20 +0300

    adjusted tests, still WIP

--- projects/libs/core-library-typescript/test/virtual-machine/ethereum-enabled-smart-contract.spec.ts
+++ projects/libs/core-library-typescript/test/virtual-machine/ethereum-enabled-smart-contract.spec.ts
@@ -2,18 +2,16 @@ import { expect } from "chai";
 import * as chai from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import "mocha";
+import { stubInterface } from "ts-sinon";
+import * as getPort from "get-port";
 
+import { EthereumSimulator } from "ethereum-simulator";
 import { StateCache } from "../../src/virtual-machine/state-cache";
 import { BaseContractStateAccessor } from "../../src/virtual-machine/contract-state-accessor";
 import EthereumConnectedSampleSmartContract from "../../src/virtual-machine/hard-coded-contracts/registry/ethereum-connected-sample-smart-contract";
-import BaseSmartContract from "../../src/virtual-machine/hard-coded-contracts/base-smart-contact";
-import { types, SidechainConnector, grpc } from "../../src";
+import { types, SidechainConnector } from "../../src";
 import { Address } from "../../src/common-library/address";
 import { createHash } from "crypto";
-import { SidechainConnectorClient } from "orbs-interfaces";
-import { EthereumSimulator } from "ethereum-simulator";
-import getPort from "get-port";
-
 
 chai.use(chaiAsPromised);
 
@@ -74,20 +72,35 @@ contract SimpleStorage {
 describe("ethereum connected contract simple example", () => {
   let adapter: ContractStateMemCacheAccessor;
   let accountContract: EthereumConnectedSampleSmartContract;
-  const SIDE_CHAIN_CONNECTOR_ENDPOINT = "http://localhost:8888";
   let ethIntValue: number;
   let ethSim: EthereumSimulator;
   let contractAddress: string;
+  // let server: GRPCServerBuilder;
+  let sidechainConnector: SidechainConnector;
 
   beforeEach(async () => {
-    adapter = new ContractStateMemCacheAccessor(CONTRACT_ADDRESS, new StateCache());
-    accountContract = new EthereumConnectedSampleSmartContract(ACCOUNT_ADDRESS, adapter, SIDE_CHAIN_CONNECTOR_ENDPOINT);
+    const sidechainEndpoint = `localhost:${await getPort()}`;
     ethIntValue = Math.floor(Math.random() * 10000000);
+
+    // start ethereum
+    const ethereumSimPort = await getPort();
     ethSim = new EthereumSimulator();
-    ethSim.listen(await getPort());
+    ethSim.listen(ethereumSimPort);
     ethSim.addContract(ETH_CONTRACT);
     ethSim.setArguments(ethIntValue, "dont-care");
     contractAddress = await ethSim.compileAndDeployContractOnGanache();
+
+    // start the sidechain connector
+    const NODE_NAME = "tester";
+    const ETHEREUM_NODE_HTTP_ADDRESS = `http://localhost:${ethereumSimPort}`;
+
+    sidechainConnector = new SidechainConnector({ nodeName: NODE_NAME, ethereumNodeHttpAddress: ETHEREUM_NODE_HTTP_ADDRESS });
+    // the only way to properly test that we instrument ethereum is to use the entire sidechain connector - we cannot stub the access interface here
+
+
+
+    adapter = new ContractStateMemCacheAccessor(CONTRACT_ADDRESS, new StateCache());
+    accountContract = new EthereumConnectedSampleSmartContract(ACCOUNT_ADDRESS, adapter, sidechainEndpoint);
   });
 
   afterEach(async () => {
@@ -109,6 +122,4 @@ describe("ethereum connected contract simple example", () => {
     const shouldBeZero = accountContract.getInt(ACCOUNT_ADDRESS);
     return expect(shouldBeZero).to.be.equal(0);
   });
-
-
 });
