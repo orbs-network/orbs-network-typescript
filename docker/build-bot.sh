38b4e900b01cf7c3add73472c7f2d6c2011b0f66
Kirill Maksimov <kirill@orbs.com>
2018-06-17 16:27:38 +0300

    Update js client sdk version

--- docker/build-bot.sh
+++ docker/build-bot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t orbs:bot -f Dockerfile.bot .
+docker build --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -t orbs:bot -f Dockerfile.bot .
