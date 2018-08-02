76545efa97d2ab5abc7216dc306eeb6fd24e0917
Jonathan Levison <jonathanl@orbs.com>
2018-08-02 16:57:16 +0300

    Make pbft consensus deployable

--- deploy/bootstrap/bootstrap.sh
+++ deploy/bootstrap/bootstrap.sh
@@ -20,8 +20,13 @@ echo ETHEREUM_NODE_HTTP_ADDRESS=$ETHEREUM_NODE_HTTP_ADDRESS >> $ENV_FILE
 
 export DOCKER_TAG=${DOCKER_TAG-master}
 # TODO: remove default image
-export DOCKER_IMAGE=${DOCKER_IMAGE-506367651493.dkr.ecr.us-west-2.amazonaws.com/orbs-network}
+# export DOCKER_IMAGE=${DOCKER_IMAGE-506367651493.dkr.ecr.us-west-2.amazonaws.com/orbs-network}
+export DOCKER_IMAGE=506367651493.dkr.ecr.us-west-2.amazonaws.com/orbs-network
 
 crontab /opt/orbs/crontab
+
+$(aws ecr get-login --no-include-email --region us-west-2)
+cp /opt/orbs/private-keys/block/secret-key /opt/orbs/private-keys/block/$NODE_NAME
+cp /opt/orbs/private-keys/message/secret-key /opt/orbs/private-keys/message/$NODE_NAME
 
 /usr/local/bin/docker-compose -f /opt/orbs/docker-compose.yml up -d
